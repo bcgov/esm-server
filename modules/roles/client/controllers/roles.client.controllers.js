cb7e28b93014c2d460d747ea8d45a1b83122cd3d
Shea Phillips <shea.phillips@gmail.com>
2016-01-05 11:50:03 -0800

    - fixed issue causing build-time jshint failure.

--- modules/roles/client/controllers/roles.client.controllers.js
+++ modules/roles/client/controllers/roles.client.controllers.js
@@ -3,7 +3,7 @@
 angular.module('roles')
     .controller('controllerPermissionMatrix', controllerPermissionMatrix)
     .filter('accessHasRole', filterAccessHasRole);
-    
+
 // -----------------------------------------------------------------------------------
 //
 // CONTROLLER: Permission Matrix
@@ -12,36 +12,38 @@ angular.module('roles')
 controllerPermissionMatrix.$inject = ['sRoles', 'rTargetObject', 'rTargetObjectType', '_', '$modalInstance'];
 /* @ngInject */
 function controllerPermissionMatrix(sRoles, rTargetObject, rTargetObjectType, _, $modalInstance) {
-	var permMatrix = this;
+    var permMatrix = this;
 
-	permMatrix.permissions = {}; //rTargetObject.access;
-	permMatrix.keys = ['read', 'write', 'watch'];
+    permMatrix.permissions = {}; //rTargetObject.access;
+    permMatrix.keys = ['read', 'write', 'watch'];
 
-	_.each(permMatrix.keys, function(key) {
-		permMatrix.permissions[key] = [];
-	});
+    _.each(permMatrix.keys, function (key) {
+        permMatrix.permissions[key] = [];
+    });
 
-	// get system roles
-	sRoles.getRoles().then( function(res) {
-	 	permMatrix.roles = res.data;
-	});
+    // get system roles
+    sRoles.getRoles().then(function (res) {
+        permMatrix.roles = res.data;
+    });
 
-	// on save, pass complete permission structure to the server
-	permMatrix.ok = function () { 
-		$modalInstance.close();
-	};
-	permMatrix.cancel = function () { $modalInstance.dismiss('cancel'); };
+    // on save, pass complete permission structure to the server
+    permMatrix.ok = function () {
+        $modalInstance.close();
+    };
+    permMatrix.cancel = function () {
+        $modalInstance.dismiss('cancel');
+    };
 
 
-	permMatrix.togglePermission = function(role, key) {
-		if (_.indexOf( permMatrix.permissions[key], role ) > -1) {
-			// it's here so remove it.
-			_.pull(permMatrix.permissions[key], role);
-		} else {
-			// it's not here, add it.
-			permMatrix.permissions[key].push(role);
-		} 
-	};
+    permMatrix.togglePermission = function (role, key) {
+        if (_.indexOf(permMatrix.permissions[key], role) > -1) {
+            // it's here so remove it.
+            _.pull(permMatrix.permissions[key], role);
+        } else {
+            // it's not here, add it.
+            permMatrix.permissions[key].push(role);
+        }
+    };
 
 }
 // -----------------------------------------------------------------------------------
@@ -52,7 +54,7 @@ function controllerPermissionMatrix(sRoles, rTargetObject, rTargetObjectType, _,
 filterAccessHasRole.$inject = ['_'];
 /* @ngInject */
 function filterAccessHasRole(_) {
-	return function(perms, role, key) {
-		return _.indexOf(perms[key], role) > -1;
-	}
+    return function (perms, role, key) {
+        return _.indexOf(perms[key], role) > -1;
+    };
 }
