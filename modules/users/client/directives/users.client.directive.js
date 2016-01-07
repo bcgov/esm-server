57a2246b725563bcabde311a74be1e67e5771944
Ben Leather <ben@3treestech.com>
2016-01-06 20:01:41 -0800

    Ajax Public Page formatting.

--- modules/users/client/directives/users.client.directive.js
+++ modules/users/client/directives/users.client.directive.js
@@ -1,8 +1,18 @@
 ﻿'use strict';
 
-// Users directive used to force lowercase input
-angular.module('users').directive('lowercase', function () {
-	return {
+angular.module('users')
+	.directive('lowercase', directiveLowercase)
+	.directive('modalUserList', directiveModalUserList);
+// -----------------------------------------------------------------------------------
+//
+// DIRECTIVE: Parser to lowercase
+//
+// -----------------------------------------------------------------------------------
+directiveLowercase.$inject = [];
+/* @ngInject */
+function directiveLowercase() {
+
+	var directive = {
 		require: 'ngModel',
 		link: function (scope, element, attrs, modelCtrl) {
 			modelCtrl.$parsers.push(function (input) {
@@ -11,4 +21,47 @@ angular.module('users').directive('lowercase', function () {
 			element.css('text-transform', 'lowercase');
 		}
 	};
-});
+	return directive;
+}
+// -----------------------------------------------------------------------------------
+//
+// DIRECTIVE: Modal Select Items
+//
+// -----------------------------------------------------------------------------------
+directiveModalUserList.$inject = ['$modal'];
+/* @ngInject */
+function directiveModalUserList($modal) {
+    var directive = {
+       	restrict:'A',
+       	scope : {
+        		users: '=',
+        		callback: '='
+		},
+		link : function(scope, element, attrs) {
+			element.on('click', function() {
+				var modalSelectItems = $modal.open({
+					animation: true,
+					templateUrl: 'modules/users/client/views/partials/nodal-user-list.html',
+					controller: 'controllerModalUserList',
+					controllerAs: 'userList',
+					resolve: {
+						rUsers: function () { // all possible options
+							return scope.users;
+						}
+					},
+					size: 'lg'
+				});
+				modalSelectItems.result.then(function (newItems) {
+					// fire callback to assign the new selections
+					// or just assign
+					if (scope.callback) {
+						scope.callback(newItems, scope.users);
+					} else {
+						scope.users = angular.copy(newItems);
+					}
+				}, function () {});
+			});
+		}
+    };
+    return directive;
+}
