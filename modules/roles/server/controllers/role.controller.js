2688eadb252904913b643db169c3f1f0669407a2
Chris Coldwell <chris.coldwell@gmail.com>
2016-01-22 16:20:52 -0800

    roles, all and centralized audit and object security

--- modules/roles/server/controllers/role.controller.js
+++ modules/roles/server/controllers/role.controller.js
@@ -9,6 +9,8 @@ var mongoose = require ('mongoose');
 var helpers  = require (path.resolve('./modules/core/server/controllers/core.helpers.controller'));
 var CRUD     = require (path.resolve('./modules/core/server/controllers/core.crud.controller'));
 var Role     = mongoose.model ('Role');
+var userController = require(path.resolve('./modules/users/server/controllers/admin.server.controller'));
+
 
 // -------------------------------------------------------------------------
 //
@@ -25,35 +27,85 @@ var saveRole = function (role) {
 // get roles (return promise)
 //
 // -------------------------------------------------------------------------
-var getRole = function (search) {
+var getRoles = function (search) {
 	return new Promise (function (resolve, reject) {
 		Role.find (search).exec().then (resolve, reject);
 	});
 };
-
-
+// -------------------------------------------------------------------------
+//
+// get project or stream roles
+//
+// -------------------------------------------------------------------------
+var getStreamRoles      = function (streamId) { return getRoles ({stream: streamId}); };
+var getProjectRoles     = function (projectId) { return getRoles ({project: projectId}); };
+// -------------------------------------------------------------------------
+//
+// copy stream roles into a project making new project roles
+// this is where naming converntion matters, to make project roles work
+// we will prefix them all with the project code. this would also mean that
+// if any objects contain this role, their names need to be changed to the
+// new name, that will hopefully get done elsewhere
+//
+// -------------------------------------------------------------------------
+var copyStreamRolesToProject = function (streamId, projectId, projectCode) {
+	return new Promise (function (resolve, reject) {
+		getStreamRoles (streamId)
+		.then (function (models) {
+			//
+			// make an array of role save promises
+			//
+			return Promise.all (models.map (function (model) {
+				var role = model.toObject ();
+				delete role._id ;
+				role         = new Role (role);
+				role.project = projectId;
+				role.stream  = null;
+				role.code    = projectCode+':'+role.code;
+				return saveRole (role);
+			}));
+		})
+		.then (resolve, reject);
+	});
+};
+// -------------------------------------------------------------------------
+//
+// add a user to a role or group (this calls add role to user)
+//
+// -------------------------------------------------------------------------
+var addUserToRole = function (req, res) {
+	userController.addUserRole (req.model, req.Role)
+	.then (function (m) {
+		helpers.sendData (res, req.Role);
+	})
+	.catch (function (err) {
+		helpers.sendError (res, err);
+	});
+};
 
 var crud = new CRUD (Role);
-// -------------------------------------------------------------------------
-//
-// Basic CRUD
-//
-// -------------------------------------------------------------------------
-exports.new    = crud.new    ();
-exports.create = crud.create ();
-exports.read   = crud.read   ();
-exports.update = crud.update ();
-exports.delete = crud.delete ();
-exports.list   = crud.list   ();
-exports.getObject   = crud.getObject   ();
-
-// exports.getRolesForProject = function (req, res) {
-// 	if (!helpers.userCan (req.user, 'read', req.Project)) {
-// 		return helpers.sendErrorMessage (res, "You do not have access to this project")
-// 	}
-// 	getRole ({
-// 		project: req.Project._id,
-// 	})
-// 	.then (helpers.successFunction (res))
-// 	.catch (helpers.errorFunction (res));
-// };
+module.exports = {
+	// -------------------------------------------------------------------------
+	//
+	// Basic CRUD
+	//
+	// -------------------------------------------------------------------------
+	new       : crud.new    (),
+	create    : crud.create (),
+	read      : crud.read   (),
+	update    : crud.update (),
+	delete    : crud.delete (),
+	list      : crud.list   (),
+	getObject : crud.getObject   (),
+	// -------------------------------------------------------------------------
+	//
+	// new stuff
+	//
+	// -------------------------------------------------------------------------
+	saveRole                 : saveRole,
+	getRoles                 : getRoles,
+	getStreamRoles           : getStreamRoles,
+	getProjectRoles          : getProjectRoles,
+	copyStreamRolesToProject : copyStreamRolesToProject,
+	addUserToRole            : addUserToRole
+};
