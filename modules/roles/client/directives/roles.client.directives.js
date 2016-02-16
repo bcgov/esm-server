644286ecd9b6086afed64d222fd8093733fbd2c6
Ben Leather <ben@3treestech.com>
2016-02-15 19:27:13 -0800

    Manage Project Users UI.

--- modules/roles/client/directives/roles.client.directives.js
+++ modules/roles/client/directives/roles.client.directives.js
@@ -1,11 +1,12 @@
 'use strict';
 
 angular.module('roles')
-    .directive('modalPermissionMatrix', directiveModalPermissionMatrix);
+    .directive('modalPermissionMatrix', directiveModalPermissionMatrix)
+    .directive('modalUsersByRoles', directiveUsersByRoles);
     
 // -----------------------------------------------------------------------------------
 //
-// DIRECTIVE: Projects Quicklinks
+// DIRECTIVE: General User Permissions
 //
 // -----------------------------------------------------------------------------------
 directiveModalPermissionMatrix.$inject = ['$modal'];
@@ -30,6 +31,38 @@ function directiveModalPermissionMatrix($modal) {
 					}
 				});
 				modalPermissionMatrix.result.then(function () {}, function () {});
+			});
+		}
+    };
+    return directive;
+}
+// -----------------------------------------------------------------------------------
+//
+// DIRECTIVE: Users by Roles
+//
+// -----------------------------------------------------------------------------------
+directiveUsersByRoles.$inject = ['$modal'];
+/* @ngInject */
+function directiveUsersByRoles($modal) {
+	var directive = {
+		restrict:'A',
+		scope : {
+			sourceObject: '='
+		},
+		link : function(scope, element, attrs) {
+			element.on('click', function() {
+				var modalUsersByRoles = $modal.open({
+					animation: true,
+					templateUrl: 'modules/roles/client/views/partials/modal-users-by-roles.html',
+					controller: 'controllerUsersByRoles',
+					controllerAs: 'usersByRoles',
+					scope: scope,
+					size: 'lg',
+					resolve: {
+						rSourceObject: function() { return scope.sourceObject; }
+					}
+				});
+				modalUsersByRoles.result.then(function () {}, function () {});
 			});
 		}
     };
