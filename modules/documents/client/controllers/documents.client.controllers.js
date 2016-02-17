8abaee2c82cc4a53a6571dc82f841b6af591f025
Mark Lise <mark.lise@gmail.com>
2016-02-16 20:58:04 -0800

    Bringing back level 2, unfolding and subtypes.

--- modules/documents/client/controllers/documents.client.controllers.js
+++ modules/documents/client/controllers/documents.client.controllers.js
@@ -112,18 +112,26 @@ function controllerDocumentUploadGlobal($scope, Upload, $timeout, Document, _) {
 		console.log("hereagain",docUpload);
 
 		if (docUpload.fileList && docUpload.fileList.length && docUpload.targetUrl) {
-			var name;
+			var name; // this is type
+			var subtype;
 			angular.forEach( docUpload.fileList, function(file) {
 				// Quick hack to pass objects
+				// TODO: Make this better
 				if (file.docType) {
 					name = file.docType.name;
 				} else {
 					name = "No Type Specified";
 				}
+				if (file.docSubType) {
+					subtype = file.docSubType.name;
+				} else {
+					subtype = "No SubType Specified";
+				}
 				file.upload = Upload.upload({
 					url: docUpload.targetUrl,
 					file: file,
-					headers: { 'documenttype': name}
+					headers: { 'documenttype': name,
+							   'documentsubtype': subtype}
 				});
 
 				file.upload.then(function (response) {
