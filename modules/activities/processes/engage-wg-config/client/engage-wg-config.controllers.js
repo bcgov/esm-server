7c1f8bd4a67e38cc1aaadf3498aa87c1ebbb2f4b
Ben Leather <ben@3treestech.com>
2016-02-24 18:28:35 -0800

    new working group engagement config activity

---
+++ modules/activities/processes/engage-wg-config/client/engage-wg-config.controllers.js
@@ -0,0 +1,15 @@
+'use strict';
+
+angular.module('process')
+	.controller('controllerProcessEngageWG', controllerProcessEngageWG);
+
+// -----------------------------------------------------------------------------------
+//
+// CONTROLLER: Process for Engage WG
+//
+// -----------------------------------------------------------------------------------
+controllerProcessEngageWG.$inject = ['$scope'];
+//
+function controllerProcessEngageWG($scope) {
+	var processEngageWG = this;
+}
