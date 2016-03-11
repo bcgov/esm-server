9c1e8b853992ead54b5fd1c9ba4e799dd6278806
Trirogoff <Jonny.Trirogoff@quartech.com>
2016-03-11 15:12:46 -0800

    Applying new routing mechanism to project conditions. Also removing modal dialogs and applying stylings to make project condition related screens consistent with other areas of the application

--- modules/project-conditions/client/config/projectcondition.routes.js
+++ modules/project-conditions/client/config/projectcondition.routes.js
@@ -1,15 +1,133 @@
 'use strict';
-
-angular.module('projectconditions').config(['$stateProvider', function ($stateProvider) {
-
+// =========================================================================
+//
+// project condition routes
+//
+// =========================================================================
+angular.module('core').config(['$stateProvider', function ($stateProvider) {
 	$stateProvider
-	.state('projectconditions', {
-		url: '/projectconditions',
-		template: '<tmpl-project-projectcondition-list></tmpl-project-projectcondition-list>',
-		data: {
-			roles: ['admin', 'user']
+	// -------------------------------------------------------------------------
+	//
+	// this is the abstract, top level view for project conditions.
+	// since it is a child of p (project), the project injection has already
+	// been resolved and is available to subsequent child states as 'project'
+	// here we will resolve the list of conditions for this project, which will
+	// also become available to child states as 'conditions'
+	//
+	// -------------------------------------------------------------------------
+	.state('p.projectcondition', {
+		abstract:true,
+		url: '/projectcondition',
+		template: '<ui-view></ui-view>',
+		resolve: {
+			conditions: function ($stateParams, ProjectConditionModel, project) {
+				console.log ('projectcondition abstract resolving conditions');
+				console.log ('project id = ', project._id);
+				return ProjectConditionModel.getConditionsForProject (project._id);
+			},
 		}
-	});
+	})
+	// -------------------------------------------------------------------------
+	//
+	// the list state for project conditions and project are guaranteed to
+	// already be resolved
+	//
+	// -------------------------------------------------------------------------
+	.state('p.projectcondition.list', {
+		url: '/list',
+		templateUrl: 'modules/project-conditions/client/views/projectcondition-list.html',
+		controller: function ($scope, NgTableParams, conditions, project) {
+			$scope.tableParams = new NgTableParams ({count:10}, {dataset: conditions});
+			$scope.project = project;
+		}
+	})
+	// -------------------------------------------------------------------------
+	//
+	// this is the add, or create state. it is defined before the others so that
+	// it does not conflict
+	//
+	// -------------------------------------------------------------------------
+	.state('p.projectcondition.create', {
+		url: '/create',
+		templateUrl: 'modules/project-conditions/client/views/projectcondition-edit.html',
+		resolve: {
+			condition: function (ProjectConditionModel) {
+				return ProjectConditionModel.getNew ();
+			}
+		},
+		controller: function ($scope, $state, project, condition, ProjectConditionModel) {
+			$scope.condition = condition;
+			$scope.project = project;
+			$scope.save = function () {
+				ProjectConditionModel.add ($scope.condition)
+				.then (function (model) {
+					$state.transitionTo('p.projectcondition.list', {project:project._id}, {
+			  			reload: true, inherit: false, notify: true
+					});
+				})
+				.catch (function (err) {
+					console.error (err);
+					alert (err);
+				});
+			};
+		}
+	})
+	// -------------------------------------------------------------------------
+	//
+	// this is the edit state
+	//
+	// -------------------------------------------------------------------------
+	.state('p.projectcondition.edit', {
+		url: '/:conditionId/edit',
+		templateUrl: 'modules/project-conditions/client/views/projectcondition-edit.html',
+		resolve: {
+			condition: function ($stateParams, ProjectConditionModel) {
+				console.log ('editing conditionId = ', $stateParams.conditionId);
+				return ProjectConditionModel.getModel ($stateParams.conditionId);
+			}
+		},
+		controller: function ($scope, $state, condition, project, ProjectConditionModel) {
+			console.log ('condition = ', condition);
+			$scope.condition = condition;
+			$scope.project = project;
+			$scope.save = function () {
+				ProjectConditionModel.save ($scope.condition)
+				.then (function (model) {
+					console.log ('condition was saved',model);
+					console.log ('now going to reload state');
+					$state.transitionTo('p.projectcondition.list', {project:project._id}, {
+			  			reload: true, inherit: false, notify: true
+					});
+				})
+				.catch (function (err) {
+					console.error (err);
+					alert (err);
+				});
+			};
+		}
+	})
+	// -------------------------------------------------------------------------
+	//
+	// this is the 'view' mode of a project condition. here we are just simply
+	// looking at the information for this specific object
+	//
+	// -------------------------------------------------------------------------
+	.state('p.projectcondition.detail', {
+		url: '/:conditionId',
+		templateUrl: 'modules/project-conditions/client/views/projectcondition-view.html',
+		resolve: {
+			condition: function ($stateParams, ProjectConditionModel) {
+				console.log ('conditionId = ', $stateParams.conditionId);
+				return ProjectConditionModel.getModel ($stateParams.conditionId);
+			}
+		},
+		controller: function ($scope, condition, project) {
+			console.log ('condition = ', condition);
+			$scope.condition = condition;
+			$scope.project = project;
+		}
+	})
+
+	;
 
 }]);
-
