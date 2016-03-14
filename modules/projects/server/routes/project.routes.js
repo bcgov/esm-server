2af4c00447e17d817eb4d77b6a6b3bcdfd91878e
Mark Lise <mark.lise@gmail.com>
2016-03-14 10:03:40 -0700

    Allow for updates to projects

--- modules/projects/server/routes/project.routes.js
+++ modules/projects/server/routes/project.routes.js
@@ -30,138 +30,91 @@ var loadProjects = function(file, req, res) {
 			Object.keys(output).forEach(function(key, index) {
 				if (index > 0) {
 					var row = output[key];
+					projectProcessed++;
 					// console.log("rowData:",row.id);
 					Model.findOne({epicProjectID: row.id}, function (err, doc) {
-						if (doc === null) {
-							projectProcessed++;
-							var p = new Project (req.user);
-							p.new().then(function(model) {
-								// console.log("MODEL:",model);
-								// LATER
-								model.epicProjectID = parseInt(row.id);
-								// console.log("epicProjectID",model.epicProjectID);
-								model.oldData = JSON.stringify(row);
-								model.status = 'In Progress';
-								model.name = row.ProjectName;
-								model.description = row.shortD;
-								model.code = model.name.toLowerCase ().replace (' ', '-').substr (0, model.name.length+1);
-								model.phases = [];
-								// _.each (phases, function (ph) {
-								// 	var phase = new Phase (ph);
-								// 	project.phases.push (phase._id);
-								// 	phase.save ();
+						var addOrChangeModel = function(model) {
+							// console.log("MODEL:",model);
+							// LATER
+							model.epicProjectID = parseInt(row.id);
+							// console.log("epicProjectID",model.epicProjectID);
+							model.oldData = JSON.stringify(row);
+							model.status = 'In Progress';
+							model.name = row.ProjectName;
+							model.description = row.shortD;
+							model.code = model.name.toLowerCase ().replace (' ', '-').substr (0, model.name.length+1);
+							model.phases = [];
+							// _.each (phases, function (ph) {
+							// 	var phase = new Phase (ph);
+							// 	project.phases.push (phase._id);
+							// 	phase.save ();
+							// });
+							model.type = row.sector;
+							Organization.findOne ({name:row.Proponent}, function (err, result) {
+								// var o = new Organization (org);
+								// o.save ().then(function (o) {
+								// 	model.proponent = o;
+								// 	model.save();
 								// });
-								model.type = row.sector;
-								Organization.findOne ({name:row.Proponent}, function (err, result) {
-									// var o = new Organization (org);
-									// o.save ().then(function (o) {
-									// 	model.proponent = o;
-									// 	model.save();
-									// });
-									if (result) {
-										console.log("RES",result);
-										model.proponent = result;
-										model.save();
-									}
-								});
-								// TODO: Remove this
-								model.region = row.Region.toLowerCase ().replace(' region','');
-								model.currentPhase = model.phases[0];
-								if (row.lat) model.lat = parseFloat(row.lat);
-								if (row.lat) model.lon = parseFloat(row.long);
-								if (row.locDescription) model.location = row.locDescription;
-								model.roles = ['mem', 'public'];
-								model.description = "";
-								model.description += 'Provincial Electoral Districts: '+row.provincialED+"\n";
-								model.description += 'Capital Investment $M: '+row.capitalInvestment+"\n";
-								model.description += 'Project File Creation Date: '+row.projectCreateDate+"\n";
-								model.description += 'Construction Jobs: '+row.constructionJobs+"\n";
-								model.description += 'Operation Jobs: '+row.operationJobs+"\n";
-								model.description += 'Sub Sector: '+row.subSector+"\n";
-								model.description += 'Active: '+row.active+"\n";
-								model.description += 'CEAA Involvement (Fed EA Req. & Type): '+row.CEAAInvolvement+"\n";
-								model.description += 'EA Issues: '+row.eaIssues+"\n";
-								model.description += 'Environmental Assessment: '+row.environmentalAssessmentNotes+"\n";
-								model.description += 'CEAA: '+row.CEAA+"\n";
-								model.description += 'Description: '+row.projectDescriptionLivingData+"\n";
-								model.description += 'firstNationsConsultation: '+row.firstNationsConsultation+"\n";
-								model.description += 'firstNationsAccess: '+row.firstNationsAccess+"\n";
-								model.description += 'firstNationsNotification: '+row.firstNationsNotification+"\n";
-								model.description += 'stakeholdersNotes: '+row.stakeholdersNotes+"\n";
-								model.description += 'federalAgencies: '+row.federalAgencies+"\n";
-								model.description += 'workingGroups: '+row.workingGroups+"\n";
-								model.description += 'allOtherStakeholderGroups: '+row.allOtherStakeholderGroups+"\n";
-								model.description += 'responsibleEPD: '+row.responsibleEPD+"\n";
-								model.description += 'projectLead: '+row.projectLead+"\n";
-								model.description += 'EAOCAARTRepresentative: '+row.EAOCAARTRepresentative+"\n";
-								model.description += 'projectOfficer: '+row.projectOfficer+"\n";
-								model.description += 'projectAnalyst: '+row.projectAnalyst+"\n";
-								model.description += 'projectAssistant: '+row.projectAssistant+"\n";
-								model.description += 'administrativeAssistant: '+row.administrativeAssistant+"\n";
-								model.description += 'CELead: '+row.CELead+"\n";
-								model.description += 'teamNotes: '+row.teamNotes+"\n";
-								model.read = ['public'];
-								model.submit = ['mem'];
-								// console.log("epicProjectID",model.epicProjectID);
-								// console.log("oldData",model.oldData);
-								// model.fillmein = row.Proponent;
-								// model.fillmein = row.Region;
-								// model.fillmein = row.shortD;
-								// model.fillmein = row.locSpatial;
-								// model.fillmein = row.locDescription;
-								// model.fillmein = row.provincialED;
-								// model.fillmein = row.federalED;
-								// model.fillmein = row.capitalInvestment;
-								// model.fillmein = row.projectCreateDate;
-								// model.fillmein = row.projectDescriptionLivingData;
-								// model.fillmein = row.tombstoneNote;
-								// model.fillmein = row.projectURL;
-								// model.fillmein = row.captialInvestmentNote;
-								// model.fillmein = row.lat;
-								// model.fillmein = row.long;
-								// model.fillmein = row.constructionJobs;
-								// model.fillmein = row.constructionJobsNotes;
-								// model.fillmein = row.operationJobs;
-								// model.fillmein = row.operationJobsNotes;
-								// model.fillmein = row.sector;
-								// model.fillmein = row.subSector;
-								// model.fillmein = row.currentPhaseTypeActivity;
-								// model.fillmein = row.active;
-								// model.fillmein = row.CEAAInvolvement;
-								// model.fillmein = row.eaIssues;
-								// model.fillmein = row.environmentalAssessmentNotes;
-								// model.fillmein = row.CEAA;
-								// model.fillmein = row.firstNationsConsultation;
-								// model.fillmein = row.firstNationsAccess;
-								// model.fillmein = row.firstNationsNotification;
-								// model.fillmein = row.stakeholdersNotes;
-								// model.fillmein = row.federalAgencies;
-								// model.fillmein = row.workingGroups;
-								// model.fillmein = row.allOtherStakeholderGroups;
-								// model.fillmein = row.responsibleEPD;
-								// model.fillmein = row.projectLead;
-								// model.fillmein = row.EAOCAARTRepresentative;
-								// model.fillmein = row.projectOfficer;
-								// model.fillmein = row.projectAnalyst;
-								// model.fillmein = row.projectAssistant;
-								// model.fillmein = row.administrativeAssistant;
-								// model.fillmein = row.CELead;
-								// model.fillmein = row.teamNotes;
-								model.save().then(function () {
-									// Am I done processing?
-									// console.log("INDEX:",index);
-									if (index === length-1) {
-										console.log("processed: ",projectProcessed);
-										res.json("{done: true, rowsProcessed: "+projectProcessed+"}");
-									}
-								});
+								if (result) {
+									model.proponent = result;
+									model.save();
+								}
 							});
+							// TODO: Remove this
+							model.region = row.Region.toLowerCase ().replace(' region','');
+							model.currentPhase = model.phases[0];
+							if (row.lat) model.lat = parseFloat(row.lat);
+							if (row.lat) model.lon = parseFloat(row.long);
+							if (row.locDescription) model.location = row.locDescription;
+							model.roles = ['mem', 'public'];
+							model.description = "";
+							model.description += 'Provincial Electoral Districts: '+row.provincialED+"\n";
+							model.description += 'Capital Investment $M: '+row.capitalInvestment+"\n";
+							model.description += 'Project File Creation Date: '+row.projectCreateDate+"\n";
+							model.description += 'Construction Jobs: '+row.constructionJobs+"\n";
+							model.description += 'Operation Jobs: '+row.operationJobs+"\n";
+							model.description += 'Sub Sector: '+row.subSector+"\n";
+							model.description += 'Active: '+row.active+"\n";
+							model.description += 'CEAA Involvement (Fed EA Req. & Type): '+row.CEAAInvolvement+"\n";
+							model.description += 'EA Issues: '+row.eaIssues+"\n";
+							model.description += 'Environmental Assessment: '+row.environmentalAssessmentNotes+"\n";
+							model.description += 'CEAA: '+row.CEAA+"\n";
+							model.description += 'Description: '+row.projectDescriptionLivingData+"\n";
+							model.description += 'firstNationsConsultation: '+row.firstNationsConsultation+"\n";
+							model.description += 'firstNationsAccess: '+row.firstNationsAccess+"\n";
+							model.description += 'firstNationsNotification: '+row.firstNationsNotification+"\n";
+							model.description += 'stakeholdersNotes: '+row.stakeholdersNotes+"\n";
+							model.description += 'federalAgencies: '+row.federalAgencies+"\n";
+							model.description += 'workingGroups: '+row.workingGroups+"\n";
+							model.description += 'allOtherStakeholderGroups: '+row.allOtherStakeholderGroups+"\n";
+							model.description += 'responsibleEPD: '+row.responsibleEPD+"\n";
+							model.description += 'projectLead: '+row.projectLead+"\n";
+							model.description += 'EAOCAARTRepresentative: '+row.EAOCAARTRepresentative+"\n";
+							model.description += 'projectOfficer: '+row.projectOfficer+"\n";
+							model.description += 'projectAnalyst: '+row.projectAnalyst+"\n";
+							model.description += 'projectAssistant: '+row.projectAssistant+"\n";
+							model.description += 'administrativeAssistant: '+row.administrativeAssistant+"\n";
+							model.description += 'CELead: '+row.CELead+"\n";
+							model.description += 'teamNotes: '+row.teamNotes+"\n";
+							model.read = ['public'];
+							model.submit = ['mem'];
+							model.save().then(function () {
+								// Am I done processing?
+								// console.log("INDEX:",index);
+								if (index === length-1) {
+									console.log("processed: ",projectProcessed);
+									res.json("{done: true, rowsProcessed: "+projectProcessed+"}");
+								}
+							});
+						};
+						if (doc === null) {
+							// Create new
+							var p = new Project (req.user);
+							p.new().then(addOrChangeModel);
 						} else {
-							// console.log("INDEX:",index);
-							if (index === length-1) {
-								console.log("processed: ",projectProcessed);
-								res.json("{done: true, rowsProcessed: "+projectProcessed+"}");
-							}
+							// Update:
+							addOrChangeModel(doc);
 						}
 					});
 				}
