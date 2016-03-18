59ff98947b691efb5695b5a2a23f1f592546473f
Mark Lise <mark.lise@gmail.com>
2016-03-18 12:33:57 -0700

    Add getRecentActivityActive()

--- modules/recent-activity/server/controllers/recent-activity.controller.js
+++ modules/recent-activity/server/controllers/recent-activity.controller.js
@@ -4,12 +4,41 @@
 // Controller for orgs
 //
 // =========================================================================
-var path     = require('path');
-var DBModel   = require (path.resolve('./modules/core/server/controllers/core.dbmodel.controller'));
-var _         = require ('lodash');
+var path        = require('path');
+var DBModel     = require (path.resolve('./modules/core/server/controllers/core.dbmodel.controller'));
+var _           = require ('lodash');
+var mongoose    = require ('mongoose');
+var Model       = mongoose.model ('RecentActivity');
 
 module.exports = DBModel.extend ({
 	name : 'RecentActivity',
-	plural : 'recentactivities'
-});
+	plural : 'recentactivities',
+    // -------------------------------------------------------------------------
+    //
+    // get activities which are active, sorted by Public Comment period, then 
+    // subsorted on type
+    //
+    // -------------------------------------------------------------------------
+    getRecentActivityActive: function () {
+        var p = Model.find ({active:true},
+                            {},
+                            {});
+        return new Promise (function (resolve, reject) {
+            //p.then (resolve, reject);
+            p.then(function (doc) {
+                // console.log("doc",doc);
+                var pcSort = _.partition(doc, { type: "Public Comment Period" });
+                var pcp = pcSort[0];
+                var news = pcSort[1];
+                var pcSortPriority = _.sortBy(pcp, function (o) {
+                    return o.priority;
+                });
+                var newsSortPriority = _.sortBy(news, function (o) {
+                    return o.priority;
+                });
 
+                resolve(pcSortPriority.concat(newsSortPriority));
+            }, reject);
+        });
+    }
+});
--- modules/recent-activity/server/routes/recent-activity.routes.js
+++ modules/recent-activity/server/routes/recent-activity.routes.js
@@ -10,5 +10,15 @@ var helpers = require ('../../../core/server/controllers/core.helpers.controller
 
 module.exports = function (app) {
 	helpers.setCRUDRoutes (app, 'recentActivity', RecentActivity, policy);
-};
 
+    //
+    // all active activities
+    //
+    app.route ('/api/recentactivity/active/list')
+        //.all (policy.isAllowed)
+        .get (function (req, res) {
+            var p = new RecentActivity ();
+            p.getRecentActivityActive ()
+            .then (helpers.success(res), helpers.failure(res));
+        });
+};
