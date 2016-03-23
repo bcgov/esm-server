40d2a5820ae2c4424790c0d423b9a58e8e72fa61
Trirogoff <Jonny.Trirogoff@quartech.com>
2016-03-23 11:44:18 -0700

    Added validation to topics - Name and Pillar are required fields. Fixed bug where pillar default value wasn't in the set list. Also fixed a bug where previous parent topic was being maintained when the pillar was changed and parent topic wasn't being set

--- modules/topics/server/models/topic.model.js
+++ modules/topics/server/models/topic.model.js
@@ -8,6 +8,6 @@ module.exports = require ('../../../core/server/controllers/core.models.controll
 .generateModel ('Topic', {
 	__codename : 'unique',
 	parent     : { type:String, default:'', index:true },
-	pillar     : { type:String, default:'Environmental', enum:['Environment', 'Economic', 'Social', 'Heritage', 'Health'] },
+	pillar     : { type:String, default:'Environment', enum:['Environment', 'Economic', 'Social', 'Heritage', 'Health'] },
 	type       : { type:String, default:'Valued Component', enum:['Valued Component', 'Pathway Component'] }
 });
