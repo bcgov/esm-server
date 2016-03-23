faa82a0b6144b8bdcceff60c799a5e98468d2a91
Ben Leather <ben@3treestech.com>
2016-03-23 11:46:42 -0700

    Replace limitWords with existing maxWords filter, Prevent return in inline edit control

--- modules/core/client/app/constants.js
+++ modules/core/client/app/constants.js
@@ -176,19 +176,4 @@ angular
 			return s;
 		};
 	})
-	.factory ('limitWords', function () {
-		return function (textToLimit, wordLimit) {
-			var finalText = "";
-			var text2 = textToLimit.replace(/\s+/g, ' ');
-			var text3 = text2.split(' ');
-			var numberOfWords = text3.length;
-			var i=0;
-			if (numberOfWords > wordLimit) {
-				for(i=0; i< wordLimit; i++)
-				finalText = finalText+" "+ text3[i]+" ";
-				return finalText+"...";
-			}
-			else return textToLimit;
-		};
-	})
 	.value('ProcessCodes', []);
