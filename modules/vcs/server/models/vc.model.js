7888fa95bcb3dd53e3da41182d6f7bf2f71be44d
Chris Coldwell <chris.coldwell@gmail.com>
2016-03-29 11:46:38 -0700

    created the proper fields for the vc model

--- modules/vcs/server/models/vc.model.js
+++ modules/vcs/server/models/vc.model.js
@@ -3,16 +3,21 @@
 //
 // Model for vcs
 //
+// in this model the topiccode is copied over from the original topic code, that
+// is how they relate back to topcs, as are the parent and pillar. these
+// are not editable, just copied. the name, however, can be edited freely and
+// the code is a free form field in this case as it is used as a tag to
+// mark vcs in all other parts of the system
+//
 // =========================================================================
 module.exports = require ('../../../core/server/controllers/core.models.controller')
 .generateModel ('Vc', {
 	__audit     : true,
-	__access    : true,
-	__tracking  : true,
+	__codename  : 'unique',
+	topiccode   : { type:String, default:'', index:true },
+	parent      : { type:String, default:'', index:true },
+	pillar      : { type:String, default:'Environment', enum:['Environment', 'Economic', 'Social', 'Heritage', 'Health'] },
 	project     : { type:'ObjectId', ref:'Project', default:null, index:true },
 	stage       : { type:String, enum:['Pre-Construction', 'Construction', 'Operations', 'Decommissioning'], default:'Operations' },
-	vc   : { type:String, default: ''},
-	complainant : { type:String, default: '' },
-	vcs         : [ { type:String } ]
 });
 
