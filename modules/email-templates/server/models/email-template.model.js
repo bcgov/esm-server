1207b3017bfb49c2159110ab279d064a929b3133
jeznorth <twenty15creative@gmail.com>
2016-04-19 14:29:15 -0700

    Merge branch 'develop' of https://github.com/bcgov/esm-server into develop
    # Please enter a commit message to explain why this merge is necessary,
    # especially if it merges an updated upstream into a topic branch.
    #
    # Lines starting with '#' will be ignored, and an empty message aborts
    # the commit.

--- modules/email-templates/server/models/email-template.model.js
+++ modules/email-templates/server/models/email-template.model.js
@@ -4,10 +4,18 @@
 // Model for orgs
 //
 // =========================================================================
+var validator = require('validator');
+var _ = require('lodash');
+
+var validateSubject = function(subject) {
+  return !_.isEmpty(subject);
+};
+
 module.exports = require ('../../../core/server/controllers/core.models.controller')
 .generateModel ('EmailTemplate', {
 	__audit  : true,
 	__codename: true,
+  subject : { type: String, validate: [validateSubject, 'Please fill in a Subject'] },
 	content  : { type: String, default:'' },
 	group  : { type: String, default:'General' }
 });
