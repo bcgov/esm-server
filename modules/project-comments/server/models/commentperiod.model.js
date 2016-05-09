a499b1088be08aeaceac4a4a900ea0b7bc431258
Chris Coldwell <chris.coldwell@gmail.com>
2016-05-09 14:33:31 -0700

    partial work on the comment period model

--- modules/project-comments/server/models/commentperiod.model.js
+++ modules/project-comments/server/models/commentperiod.model.js
@@ -6,22 +6,65 @@
 // =========================================================================
 module.exports = require ('../../../core/server/controllers/core.models.controller')
 .generateModel ('CommentPeriod', {
-	__audit          : true,
-	__access         : true,
-	__tracking       : true,
+	//
+	// for access, if the period is NOT published, then there should be no way
+	// to get to a list of comments. it is possible for comments to be published,
+	// but not the period, this would mean the overall link would not show, but if
+	// a user had a link to a specific comment, they could still follow that
+	//
+	__audit          : true, // who did what when
+	__access         : true, // who can mess with this and how
+	__tracking       : true, // start and stop dates
 	periodType       : { type:String, default:'Working Group', enum:['Working Group', 'Public']},
+	//
+	// what project and phase this comes under. the milestone actually has all this
+	// information, but putting them all at this level is for query optimization
+	//
 	project          : { type:'ObjectId', ref:'Project', default:null, index:true },
 	phase            : { type:'ObjectId', ref:'Phase', default:null, index:true },
+	milestone        : { type:'ObjectId', ref:'Milestone', default:null, index:true },
+	//
+	// for query optimization
+	//
 	phaseName        : { type:String, default: '' },
-	artifactName     : { type:String, default: '' },
-	artifactVersion  : { type:String, default: '' },
-	artifactVersionNumber        : { type:String, default:'',  index:true},
-	artifactTypeCode : { type:String, default: '' },
+	//
+	// these three define the target of this period
+	//
 	artifact         : { type:'ObjectId', ref:'Artifact' },
+	artifactVersion  : { type:String, default: '' },
+	artifactVersionNumber  : { type:String, default:'',  index:true},
+	//
+	// these are surfaced for query optimization
+	//
+	artifactName     : { type:String, default: '' },
+	artifactTypeCode : { type:String, default: '' },
+	//
+	// commenter roles, a list of roles that are allowed to post comments
+	// if this were a public period this would simply be 'public'
+	//
+	commenterRoles : [ String ]
+	// -------------------------------------------------------------------------
+	//
+	// these are specific to working group comments
+	//
+	// -------------------------------------------------------------------------
+	//
+	// instructions to the commenters
+	//
 	instructions     : { type:String, default: '' },
+	//
+	// is this entire comment period considered resolved, this is a calculated
+	// field, derived from AND ing the corresponding field in ALL comments
+	// the resolved percent below is also derived in the same fashion as a percent
+	// of all comments which are resolved
+	//
 	isResolved         : { type:Boolean, default:false },
 	resolvedPercent  : { type:Number, default:0.0 },
-	// published        : { type:Boolean, default:false }, we have isPublished in __access
+	// -------------------------------------------------------------------------
+	//
+	// these are specific to public comments
+	//
+	// -------------------------------------------------------------------------
 	publishedPercent : { type:Number, default:0.0 },
 	isVetted        : { type:Boolean, default:false },
 	vettedPercent : { type:Number, default:0.0 },
@@ -29,7 +72,7 @@ module.exports = require ('../../../core/server/controllers/core.models.controll
 	classifiedPercent : { type:Number, default:0.0 }
 },
 {
-	roles: 1
+	commenterRoles: 1
 }
 );
 
