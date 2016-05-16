7036c5360a6a777f3e9d3ef23cfe353f2593b52d
Mark Lise <mark.lise@gmail.com>
2016-05-16 11:16:09 -0700

    Remove filter for now

--- modules/artifacts/client/directives/artifact.directives.js
+++ modules/artifacts/client/directives/artifact.directives.js
@@ -34,11 +34,11 @@ angular.module('artifacts')
 			this.init = function () {
 				// In this view we don't want individual VC's to show up, instead they will
 				// show up in the VC page.
-				ArtifactModel.forProjectFilterType ($scope.project._id, "valued-component").then (function (c) {
-					s.tableParams = new NgTableParams ({count:10}, {dataset: c});
-					// console.log ("artifacts = ", c);
-					$scope.$apply ();
-				});
+				// ArtifactModel.forProjectFilterType ($scope.project._id, "valued-component").then (function (c) {
+				// 	s.tableParams = new NgTableParams ({count:10}, {dataset: c});
+				// 	// console.log ("artifacts = ", c);
+				// 	$scope.$apply ();
+				// });
 				if (!s.public) {
 					ArtifactModel.availableTypes ($scope.project._id).then (function (c) {
 						s.availableTypes = c;
