4b39cc84518974d2333450946ca82911e41a4fda
Jonny Trirogoff <jonny.trirogoff@quartech.com>
2016-05-20 14:55:31 -0700

    Adding template changes to seed data: ESM-212, ESM-213, ESM-214

--- config/lib/seed.js
+++ config/lib/seed.js
@@ -125,7 +125,7 @@ checkIntegration ('loadconditions2').then (function () {
 // artifact types
 //
 // -------------------------------------------------------------------------
-checkIntegration ('loadartifacts52').then (function () {
+checkIntegration ('loadartifacts53').then (function () {
 	require('../seed-data/loadartifacts')();
 });
 // -------------------------------------------------------------------------
