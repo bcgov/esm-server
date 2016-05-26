f470e6baa1731c32a60aa0e7c1ec454fe2bf6f91
Chris Coldwell <chris.coldwell@gmail.com>
2016-05-26 12:01:33 -0700

    changes for public commenting, fixing incorrect references to roles instead of commenter roles

--- modules/project-comments/server/controllers/commentperiod.controller.js
+++ modules/project-comments/server/controllers/commentperiod.controller.js
@@ -32,13 +32,13 @@ module.exports = DBModel.extend ({
 				//
 				// add the base milestone for public comments
 				//
-				return phaseModel.addMilestone (phase, 'public-comment-period', {write:period.roles});
+				return phaseModel.addMilestone (phase, 'public-comment-period', {write:period.commenterRoles});
 			}
 			else if (period.periodType === 'Working Group') {
 				//
 				// add the base milestone for working group comments
 				//
-				return phaseModel.addMilestone (phase, 'comment-period', {write:period.roles});
+				return phaseModel.addMilestone (phase, 'comment-period', {write:period.commenterRoles});
 			}
 		})
 		.then (function (phase) {
@@ -69,6 +69,11 @@ module.exports = DBModel.extend ({
 			return period;
 		});
 	},
+	// -------------------------------------------------------------------------
+	//
+	// get all comment periods for a project
+	//
+	// -------------------------------------------------------------------------
 	getForProject: function (projectId) {
 		return this.findMany ({project:projectId});
 	},
@@ -103,19 +108,19 @@ module.exports = DBModel.extend ({
 		var update;
 		if (value) {
 			update = {
-				published: true,
+				isPublished: true,
 				$addToSet: {read: 'public'}
 			};
 		} else {
 			update = {
-				published: false,
+				isPublished: false,
 				$pull: {read: 'public'}
 			};
 		}
 		return new Promise (function (resolve, reject) {
 			Comment.update (query, update, {multi: true}).exec()
 			.then (function () {
-				commentPeriod.set ({ published: value });
+				commentPeriod.set ({ isPublished: value });
 				return self.saveDocument (commentPeriod);
 			})
 			.then (resolve, reject);
