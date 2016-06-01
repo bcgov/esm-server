738ebd297237164920637dc15d6602c4e2e8dcbe
Trirogoff <Jonny.Trirogoff@quartech.com>
2016-06-01 14:02:38 -0700

    Adding seed data for new Valued Components template and modifying rich textbox template control

--- config/lib/seed.js
+++ config/lib/seed.js
@@ -125,7 +125,7 @@ checkIntegration ('loadconditions2').then (function () {
 // artifact types
 //
 // -------------------------------------------------------------------------
-checkIntegration ('loadartifacts55').then (function () {
+checkIntegration ('loadartifacts56').then (function () {
 	require('../seed-data/loadartifacts')();
 });
 // -------------------------------------------------------------------------
