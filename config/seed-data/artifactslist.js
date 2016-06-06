1a5b13feccc185245222ef55c2c1ae42696f93f3
Mark Lise <mark.lise@gmail.com>
2016-06-06 09:38:24 -0700

    More cleanup

--- config/seed-data/artifactslist.js
+++ config/seed-data/artifactslist.js
@@ -100,15 +100,15 @@ var artifacttypes = [{
         'Final'
     ]
 },{
-    name: 'CC Test Templated',
-    code: 'cc-test-template',
-    milestone: 'section-11-order',
-    multiple: false,
-    versions : [
-        'Draft',
-        'Final'
-    ]
-},{
+     name: 'Section 11 Schedule A',
+     milestone: 'section-11-schedule-a',
+     code: 'section-11-schedule-a',
+     multiple: false,
+     versions : [
+         'Draft',
+         'Final'
+     ]
+ },{
     name: 'Section 34 Order',
     milestone: 'section-34-order',
     code: 'section-34-order',
