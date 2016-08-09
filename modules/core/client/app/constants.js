fec3b29051b55d7a86dac86c4ad11f124c80a94d
Mark Lise <mark.lise@gmail.com>
2016-08-09 11:23:44 -0700

    Re-enabling NON-GMTR routes.

--- modules/core/client/app/constants.js
+++ modules/core/client/app/constants.js
@@ -187,12 +187,12 @@ angular
 	})
 	.constant('RELEASE',
 		{
-			'enableEnforcements': false,
-			'enableDecisions': false,
-			'enableSchedule': false,
-			'enableComplaints': false,
-			'enableConditions': false,
-			'enableInspectionReports': false,
+			'enableEnforcements': true,
+			'enableDecisions': true,
+			'enableSchedule': true,
+			'enableComplaints': true,
+			'enableConditions': true,
+			'enableInspectionReports': true,
 			'redirectHomepageToGeorgeMassey': false
 		}
 	)
