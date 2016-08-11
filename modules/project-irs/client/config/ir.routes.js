9ebd0b91d2db7b21c8992d12a7c6a68a9c8df0e5
Mark Lise <mark.lise@gmail.com>
2016-08-10 21:52:16 -0700

    Inspection reports now create artifact templates instead of the ir model.

--- modules/project-irs/client/config/ir.routes.js
+++ modules/project-irs/client/config/ir.routes.js
@@ -21,8 +21,8 @@ angular.module('irs').config(['$stateProvider', 'RELEASE', function ($stateProvi
 			url: '/ir',
 			template: '<ui-view></ui-view>',
 			resolve: {
-				irs: function ($stateParams, IrModel, project) {
-					return IrModel.forProject (project._id);
+				irs: function ($stateParams, ArtifactModel, project) {
+					return ArtifactModel.forProjectGetType(project._id, "inspection-report");
 				},
 			}
 		})
@@ -48,102 +48,13 @@ angular.module('irs').config(['$stateProvider', 'RELEASE', function ($stateProvi
 		// -------------------------------------------------------------------------
 		.state('p.ir.create', {
 			url: '/create',
-			templateUrl: 'modules/project-irs/client/views/ir-edit.html',
-			resolve: {
-				ir: function (IrModel, project) {
-					return new Promise( function (resolve, reject) {
-						IrModel.getNew().then(function (obj) {
-							obj.project = project;
-							resolve(obj);
-						});
-					});
-				},
-				report: function (InspectionReportModel, project) {
-					return new Promise( function (resolve, reject) {
-						InspectionReportModel.getNew().then(function (obj) {
-							obj.projectName			= project.name;
-							obj.region				= project.region;
-							obj.lat					= project.lat;
-							obj.lon					= project.lon;
-							obj.sector				= project.sector;
-							obj.locationDescription = project.location;
-							// console.log("obj",obj);
-							resolve(obj);
-						});
-					});
-				}
-			},
-			controller: function ($scope, $state, project, ir, IrModel, report, InspectionReportModel) {
-				$scope.ir = ir;
-				$scope.report = report;
-				$scope.project = project;
-				$scope.save = function () {
-					IrModel.add ($scope.ir)
-					.then (function (model) {
-						$state.transitionTo('p.ir.list', {projectid:project.code}, {
-							reload: true, inherit: false, notify: true
-						});
-					})
-					.catch (function (err) {
-						console.error (err);
-						alert (err);
-					});
-				};
-			}
-		})
-		// -------------------------------------------------------------------------
-		//
-		// this is the edit state
-		//
-		// -------------------------------------------------------------------------
-		.state('p.ir.edit', {
-			url: '/:irId/edit',
-			templateUrl: 'modules/project-irs/client/views/ir-edit.html',
-			resolve: {
-				ir: function ($stateParams, IrModel) {
-					// console.log ('editing irId = ', $stateParams.irId);
-					return IrModel.getModel ($stateParams.irId);
-				}
-			},
-			controller: function ($scope, $state, ir, project, IrModel) {
-				// console.log ('ir = ', ir);
-				$scope.ir = ir;
-				$scope.project = project;
-				$scope.save = function () {
-					IrModel.save ($scope.ir)
-					.then (function (model) {
-						// console.log ('ir was saved',model);
-						// console.log ('now going to reload state');
-						$state.transitionTo('p.ir.list', {projectid:project.code}, {
-							reload: true, inherit: false, notify: true
-						});
-					})
-					.catch (function (err) {
-						console.error (err);
-						alert (err);
-					});
-				};
-			}
-		})
-		// -------------------------------------------------------------------------
-		//
-		// this is the 'view' mode of an ir. here we are just simply
-		// looking at the information for this specific object
-		//
-		// -------------------------------------------------------------------------
-		.state('p.ir.detail', {
-			url: '/:irId',
-			templateUrl: 'modules/project-irs/client/views/ir-view.html',
-			resolve: {
-				ir: function ($stateParams, IrModel) {
-					// console.log ('irId = ', $stateParams.irId);
-					return IrModel.getModel ($stateParams.irId);
-				}
-			},
-			controller: function ($scope, ir, project) {
-				// console.log ('ir = ', ir);
-				$scope.ir = ir;
-				$scope.project = project;
+			template: '<p></p>',
+			controller: function ($state, project, ArtifactModel) {
+				ArtifactModel.newFromType("inspection-report", project._id)
+				.then (function (a) {
+					// console.log ('artifact = ', a);
+					$state.go ('p.artifact.edit', {artifactId:a._id});
+				});
 			}
 		});
 	}
