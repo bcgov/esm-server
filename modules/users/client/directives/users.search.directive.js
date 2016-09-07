5a253ee248494ef0cad674d771ff70107a19d9c8
jeznorth <twenty15creative@gmail.com>
2016-09-07 11:17:01 -0700

    Fixed a small issue with selectable table rows not rendering correctly

--- modules/users/client/directives/users.search.directive.js
+++ modules/users/client/directives/users.search.directive.js
@@ -1,6 +1,23 @@
 'use strict';
 
 angular.module('users')
+
+	.directive('ngEnter', function () { //a directive to 'enter key press' in elements with the "ng-enter" attribute
+
+		return function (scope, element, attrs) {
+
+			element.bind("keydown keypress", function (event) {
+				if (event.which === 13) {
+					scope.$apply(function () {
+						scope.$eval(attrs.ngEnter);
+					});
+
+					event.preventDefault();
+				}
+			});
+		};
+	})
+
 	.directive('userSearchChooser', function ($filter, $modal, NgTableParams, ProjectGroupModel, UserModel, _) {
 		return {
 			restrict: 'A',
