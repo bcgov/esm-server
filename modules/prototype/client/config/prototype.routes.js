c6bf1c93927d77b77dc81c9f7184f8f593211a20
jeznorth <twenty15creative@gmail.com>
2016-11-10 11:20:18 -0800

    Prototype Development - continued work on routes / mock-up data - moved styles & images to /prototype directory

--- modules/prototype/client/config/prototype.routes.js
+++ modules/prototype/client/config/prototype.routes.js
@@ -37,5 +37,95 @@ angular.module('prototype').config(['$stateProvider', function ($stateProvider) 
 		},
 	})
 
+	// PROJECT MAIN 
+	.state('admin.prototype.projectmain', {
+		url: '/project-main',
+		templateUrl: 'modules/prototype/client/views/project-main.html',
+		controller: function ($scope, NgTableParams, Application, Authentication, PrototypeModel) {
+			$scope.authentication = Authentication;
+			$scope.application = Application;
+
+			// Project Detail (Main)
+			$scope.projectDetail = PrototypeModel.getProjectDetail();
+
+			// Project Authorizations
+			$scope.projectAuthorizations = PrototypeModel.getProjectAuthorizations();
+
+			// Project Inspections
+			$scope.projectInspections = PrototypeModel.getProjectInspections();
+
+			// TODO: Need a count all actions across all inspections per project
+			$scope.projectActionCount = 0;
+
+			// Project Details List (aside)
+			$scope.projectDetailList = PrototypeModel.getProjectDetailList();
+
+			// Project Status List (aside)
+			$scope.projectStatusList = PrototypeModel.getProjectStatusList();
+			console.log("data:", $scope.data);
+		},
+	})
+
+	// COMPLIANCE & ENFORCEMENT MAIN 
+	.state('admin.prototype.cemain', {
+		url: '/compliance-and-enforcement',
+		templateUrl: 'modules/prototype/client/views/ce-main.html',
+		controller: function ($scope, NgTableParams, Application, Authentication, PrototypeModel) {
+			$scope.authentication = Authentication;
+			$scope.application = Application;
+			
+			// Project Inspections
+			$scope.projectInspections = PrototypeModel.getProjectInspections();
+
+			console.log("data:", $scope.data);
+		},
+	})
+
+	// INSPECTION 
+	.state('admin.prototype.ceInspection', {
+		url: '/inspection',
+		templateUrl: 'modules/prototype/client/views/ce-inspection.html',
+		controller: function ($scope, NgTableParams, Application, Authentication, PrototypeModel) {
+			$scope.authentication = Authentication;
+			$scope.application = Application;
+			console.log("data:", $scope.data);
+		},
+	})
+
+	// ACTION 
+	.state('admin.prototype.ceAction', {
+		url: '/action',
+		templateUrl: 'modules/prototype/client/views/ce-action.html',
+		controller: function ($scope, NgTableParams, Application, Authentication, PrototypeModel) {
+			$scope.authentication = Authentication;
+			$scope.application = Application;
+			console.log("data:", $scope.data);
+		},
+	})
+
+	// TOPIC
+	.state('admin.prototype.topic', {
+		url: '/topic',
+		templateUrl: 'modules/prototype/client/views/topic.html',
+		controller: function ($scope, NgTableParams, Application, Authentication, PrototypeModel) {
+			$scope.authentication = Authentication;
+			$scope.application = Application;
+			console.log("data:", $scope.data);
+		},
+	})
+
+	// SUBTOPIC
+	.state('admin.prototype.subtopic', {
+		url: '/subtopic',
+		templateUrl: 'modules/prototype/client/views/subtopic.html',
+		controller: function ($scope, NgTableParams, Application, Authentication, PrototypeModel) {
+			$scope.authentication = Authentication;
+			$scope.application = Application;
+			console.log("data:", $scope.data);
+		},
+	})
+
 	;
+
+
 }]);
--- modules/prototype/client/services/prototype.clients.services.js
+++ modules/prototype/client/services/prototype.clients.services.js
@@ -8,7 +8,131 @@ angular.module('prototype').factory ('PrototypeModel', function (ModelBase, _) {
 		urlName : 'prototype',
 		getData: function () {
 			return [{name: 'test', data: 'foo'}];
-		}
+		},
+
+		getProjectDetail: function () {
+			return
+		},
+
+		getProjectDetailList: function () {
+			return [
+				{name: 'Operator', data: 'Thompson Creek Metals Company Inc.'},
+				{name: 'Ownership', data: 'Thompson Creek Metals Company Inc.'},
+				{name: 'Commodities', data: 'Copper, Gold'},
+				{name: 'Tailings Impoundments', data: '1'}
+			];
+		},
+
+		getProjectAuthorizations: function () {
+			return [
+				{name: 'Environmental Assessment Certificate', agency: 'Environmental Assessment Office (EAO)', state: 'Certificate Amended', lastUpdate: 'YYYY-MM-DD'},
+				{name: 'Environmental Management Act Permit', agency: 'Ministry of Environment (MOE)', state: 'Permit Amended', lastUpdate: 'YYYY-MM-DD'},
+				{name: 'Mines Act Permit', agency: 'Ministry of Energy & Mines (MEM)', state: 'Permit Amended', lastUpdate: 'YYYY-MM-DD'}
+			];
+		},
+
+		getProjectInspections: function () {
+			return [
+				{	inspectionID: 'MEM-63456', inspectionDate: 'YYYY-MM-DD', agency: 'Ministry of Energy & Mines',
+					inspectionTopics: [
+						{topicName: 'Topic 1'},
+						{topicName: 'Topic 2'},
+						{topicName: 'Topic 3'}
+					],
+					inspectionActions: [
+						{actionID: '1'},
+						{actionID: '2'}, 
+						{actionID: '3'}, 
+						{actionID: '4'}
+					]
+				},
+				{	inspectionID: 'MEM-63457', inspectionDate: 'YYYY-MM-DD', agency: 'Ministry of Energy & Mines',
+					inspectionTopics: [
+						{topicName: 'Topic 4'},
+						{topicName: 'Topic 5'},
+						{topicName: 'Topic 6'}
+					],
+					inspectionActions: [
+						{actionID: '1'},
+						{actionID: '2'}, 
+						{actionID: '3'}, 
+						{actionID: '4'}
+					]
+				},
+				{	inspectionID: 'MEM-63458', inspectionDate: 'YYYY-MM-DD', agency: 'Ministry of Energy & Mines',
+					inspectionTopics: [
+						{topicName: 'Topic 4'},
+						{topicName: 'Topic 5'},
+						{topicName: 'Topic 6'}
+					],
+					inspectionActions: [
+						{actionID: '1'},
+						{actionID: '2'}, 
+						{actionID: '3'}, 
+						{actionID: '4'}
+					]
+				},
+				{	inspectionID: 'MEM-63458', inspectionDate: 'YYYY-MM-DD', agency: 'Ministry of Energy & Mines',
+					inspectionTopics: [
+						{topicName: 'Topic 4'},
+						{topicName: 'Topic 5'},
+						{topicName: 'Topic 6'}
+					],
+					inspectionActions: [
+						{actionID: '1'},
+						{actionID: '2'}, 
+						{actionID: '3'}, 
+						{actionID: '4'}
+					]
+				},
+				{	inspectionID: 'MEM-63460', inspectionDate: 'YYYY-MM-DD', agency: 'Ministry of Energy & Mines',
+					inspectionTopics: [
+						{topicName: 'Topic 4'},
+						{topicName: 'Topic 5'},
+						{topicName: 'Topic 6'}
+					],
+					inspectionActions: [
+						{actionID: '1'},
+						{actionID: '2'}, 
+						{actionID: '3'}, 
+						{actionID: '4'}
+					]
+				},
+				{	inspectionID: 'MEM-63461', inspectionDate: 'YYYY-MM-DD', agency: 'Ministry of Energy & Mines',
+					inspectionTopics: [
+						{topicName: 'Topic 4'},
+						{topicName: 'Topic 5'},
+						{topicName: 'Topic 6'}
+					],
+					inspectionActions: [
+						{actionID: '1'},
+						{actionID: '2'}, 
+						{actionID: '3'}, 
+						{actionID: '4'}
+					]
+				}
+			];
+		},
+
+		getProjectStatusList: function () {
+			return [
+				{name: 'Design', status: 'Complete'},
+				{name: 'Construction', status: 'Complete'},
+				{name: 'Operation', status: 'Active'},
+				{name: 'Closure', status: 'Inactive'},
+				{name: 'Reclamation', status: 'Active'},
+				{name: 'Monitoring & Reporting', status: 'Active'},
+			];
+		},
+
+		getRelatedDocuments: function () {
+			return [
+				{documentName: 'Document Name 1', url: '#'},
+				{documentName: 'Document Name 2', url: '#'},
+				{documentName: 'Document Name 3', url: '#'},
+			];
+		},
+		
 	});
 	return new PrototypeModel ();
 });
