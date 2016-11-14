8d1344366c46a42f8ec88e794ca7f92d34dbbeb5
jsher <jason@sure.mn>
2016-11-14 14:53:21 -0800

    PROTOTYPE: json in db.

--- modules/prototype/client/services/prototype.clients.services.js
+++ modules/prototype/client/services/prototype.clients.services.js
@@ -1,13 +1,10 @@
 'use strict';
 angular.module('prototype').factory ('PrototypeModel', function (ModelBase, _) {
-	var prototypeJson = {};
-
 
 	var PrototypeModel = ModelBase.extend ({
 		urlName : 'prototype',
 		getData: function() {
-			//return this.get('/api/admin/prototype');
-			return prototypeJson;
+			return this.get('/api/admin/prototype');
 		}
 		
 	});
