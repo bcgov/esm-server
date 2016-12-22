6f6d648e21275529adc0e0ca6ca94edeed840bd3
jsher <jason@sure.mn>
2016-12-21 16:09:25 -0800

    ESM-665: remove old Documents menu, remove old Upload Documents button from Link Documents.

--- modules/documents/client/config/documents.routes.js
+++ modules/documents/client/config/documents.routes.js
@@ -3,15 +3,6 @@
 angular.module('documents').config(['$stateProvider', function ($stateProvider) {
 
 	$stateProvider
-	.state('p.documents', {
-		url: '/documents',
-		templateUrl: 'modules/documents/client/views/documents.html',
-		data: { },
-		controller: function($scope, project) {
-			$scope.project = project;
-		}
-
-	})
 	.state('p.docs', {
 		url: '/docs',
 		templateUrl: 'modules/documents/client/views/docs.html',
