f2452652595c184270528d3f899f35db521d0a69
jsher <jason@sure.mn>
2016-12-28 16:05:43 -0800

    MEM-182: Add new user/contact import version.

--- modules/users/server/controllers/users/users.import.server.controller.js
+++ modules/users/server/controllers/users/users.import.server.controller.js
@@ -60,6 +60,7 @@ exports.update = function (req, res) {
 
 // Import a list of users
 exports.loadUsers = function(file, req, res, opts) {
+	var OrgCtrl = new OrganizationController(opts);
 	return new Promise (function (resolve, reject) {
 		// Now parse and go through this thing.
 		fs.readFile(file.path, 'utf8', function(err, data) {
@@ -68,7 +69,10 @@ exports.loadUsers = function(file, req, res, opts) {
 			}
 			var v1ColArray = ['PERSON_ID','EAO_STAFF_FLAG','PROPONENT_FLAG','SALUTATION','FIRST_NAME','MIDDLE_NAME','LAST_NAME','TITLE','ORGANIZATION_NAME','DEPARTMENT','EMAIL_ADDRESS','PHONE_NUMBER','HOME_PHONE_NUMBER','FAX_NUMBER','CELL_PHONE_NUMBER','ADDRESS_LINE_1','ADDRESS_LINE_2','CITY','PROVINCE_STATE','COUNTRY','POSTAL_CODE','NOTES'];
 			var v1RowToObject = function(row) {
-				// console.log('v1: row = ', row);
+				//console.log('v1: row = ', row);
+				if (_.isEmpty(row.FIRST_NAME)) {
+					return null;
+				}
 				row.EMAIL_ADDRESS = row.EMAIL_ADDRESS ? row.EMAIL_ADDRESS.trim() : "";
 				var obj = {
 					personId      : parseInt(row.PERSON_ID),
@@ -81,7 +85,7 @@ exports.loadUsers = function(file, req, res, opts) {
 					phoneNumber   : row.PHONE_NUMBER,
 					homePhoneNumber : row.HOME_PHONE_NUMBER,
 					email         : row.EMAIL_ADDRESS,
-					viaEmail      : row.EMAIL_ADDRESS !== "",
+					viaEmail      : !_.isEmpty(row.EMAIL_ADDRESS),
 					eaoStaffFlag  : Boolean(row.EAO_STAFF_FLAG),
 					proponentFlag : Boolean(row.PROPONENT_FLAG),
 					salutation    : row.SALUTATION,
@@ -94,18 +98,21 @@ exports.loadUsers = function(file, req, res, opts) {
 					province      : row.PROVINCE_STATE,
 					country       : row.COUNTRY,
 					postalCode    : row.POSTAL_CODE,
-					viaMail       : row.ADDRESS_LINE_1 !== "",
+					viaMail       : _.isEmpty(row.EMAIL_ADDRESS) && !_.isEmpty(row.ADDRESS_LINE_1),
 					notes         : row.NOTES,
 					username      : row.EMAIL_ADDRESS !== "" ? row.EMAIL_ADDRESS : row.FIRST_NAME +"."+ row.LAST_NAME +"."+ row.ORGANIZATION_NAME,
 					password      : crypto.randomBytes(8)
 				};
-				// console.log('v1: obj = ', JSON.stringify(obj, null, 4));
+				//console.log('v1: obj = ', JSON.stringify(obj, null, 4));
 				return obj;
 			};
 			
 			var v2ColArray = _.concat(v1ColArray, 'DISPLAY_NAME', 'USERNAME', 'PASSWORD', 'SALT');
 			var v2RowToObject = function(row) {
-				// console.log('v2: row = ', row);
+				//console.log('v2: row = ', row);
+				if (_.isEmpty(row.FIRST_NAME)) {
+					return null;
+				}
 				row.EMAIL_ADDRESS = row.EMAIL_ADDRESS ? row.EMAIL_ADDRESS.trim() : "";
 				var obj = {
 					personId      : parseInt(row.PERSON_ID),
@@ -118,7 +125,7 @@ exports.loadUsers = function(file, req, res, opts) {
 					phoneNumber   : row.PHONE_NUMBER,
 					homePhoneNumber : row.HOME_PHONE_NUMBER,
 					email         : row.EMAIL_ADDRESS,
-					viaEmail      : row.EMAIL_ADDRESS !== "",
+					viaEmail      : !_.isEmpty(row.EMAIL_ADDRESS),
 					eaoStaffFlag  : Boolean(row.EAO_STAFF_FLAG),
 					proponentFlag : Boolean(row.PROPONENT_FLAG),
 					salutation    : row.SALUTATION,
@@ -131,137 +138,207 @@ exports.loadUsers = function(file, req, res, opts) {
 					province      : row.PROVINCE_STATE,
 					country       : row.COUNTRY,
 					postalCode    : row.POSTAL_CODE,
-					viaMail       : row.ADDRESS_LINE_1 !== "",
+					viaMail       : _.isEmpty(row.EMAIL_ADDRESS) && !_.isEmpty(row.ADDRESS_LINE_1),
 					notes         : row.NOTES,
 					username      : row.USERNAME,
 					password      : row.PASSWORD,
 					salt          : row.SALT
 				};
-				// console.log('v2: obj = ', JSON.stringify(obj, null, 4));
+				//console.log('v2: obj = ', JSON.stringify(obj, null, 4));
 				return obj;
 			};
-			
-			
+			var v3ColArray = ['SALUTATION','FIRST_NAME','MIDDLE_NAME','LAST_NAME','DISPLAY_NAME','USERNAME','VIA_EMAIL','VIA_MAIL','EMAIL','PHONE','MOBILE','FAX','ADDRESS_1','ADDRESS_2','CITY','PROVINCE','COUNTRY','POSTAL','ORGANIZATION_NAME','DEPARTMENT','TITLE','NOTES','HOME_PHONE','PERSON_ID','EAO_STAFF_FLAG','PROPONENT_FLAG','PASSWORD'];
+			var v3RowToObject = function(row) {
+				//console.log('v3: row = ', row);
+				if (_.isEmpty(row.FIRST_NAME)) {
+					return null;
+				}
+				var obj = {
+					salutation    : row.SALUTATION,
+					firstName     : row.FIRST_NAME,
+					middleName    : row.MIDDLE_NAME,
+					lastName      : row.LAST_NAME,
+					displayName   : row.DISPLAY_NAME,
+					username      : row.USERNAME,
+					viaEmail      : _.isEmpty(row.VIA_EMAIL) ? false : Boolean(row.VIA_EMAIL),
+					viaMail       : _.isEmpty(row.VIA_MAIL) ? false : Boolean(row.VIA_MAIL),
+					email         : row.EMAIL ? row.EMAIL.trim() : "",
+					phoneNumber   : row.PHONE,
+					cellPhoneNumber : row.MOBILE,
+					faxNumber     : row.FAX_NUMBER,
+					address1      : row.ADDRESS_1,
+					address2      : row.ADDRESS_2,
+					city          : row.CITY,
+					province      : row.PROVINCE,
+					country       : row.COUNTRY,
+					postalCode    : row.POSTAL,
+					orgName       : row.ORGANIZATION_NAME,
+					department    : row.DEPARTMENT,
+					title         : row.TITLE,
+					notes         : row.NOTES,
+					homePhoneNumber : row.HOME_PHONE,
+					personId      : _.isEmpty(row.PERSON_ID) ? null: parseInt(row.PERSON_ID),
+					eaoStaffFlag  : _.isEmpty(row.EAO_STAFF_FLAG) ? false : Boolean(row.EAO_STAFF_FLAG),
+					proponentFlag : _.isEmpty(row.PROPONENT_FLAG) ? false : Boolean(row.PROPONENT_FLAG),
+					password      : _.isEmpty(row.PASSWORD) ? crypto.randomBytes(8): row.PASSWORD
+				};
+				//console.log('v3: obj = ', JSON.stringify(obj, null, 4));
+				return obj;
+			};
+
+
 			var lines = data.split(/\r\n|\r|\n/g);
-			// console.log('File line count =  ', _.size(lines));
-			var v1 = _.size(lines) === 0 ? true : (lines[0].match(/,/g).length === v1ColArray.length-1);
-			// console.log('File v1? ', v1);
-			
-			var colArray = v1 ? v1ColArray : v2ColArray;
-			var rowParser = v1 ? v1RowToObject : v2RowToObject;
+			//console.log('File line count =  ', _.size(lines));
+			var importVersion = 'v1';
+			var colArray = v1ColArray;
+			var rowParser = v1RowToObject;
 
-			var parse = new CSVParse(data, {delimiter: ',', columns: colArray}, function(err, output){
-				// Skip this many rows
-				var length = Object.keys(output).length;
-				var promises = [];
-				// console.log("length",length);
-				Object.keys(output).forEach(function(key, index) {
-					if (index > 0) {
-						var row = output[key];
-						promises.push(rowParser(row));
+			var checkImportVersion = function() {
+				importVersion = 'unknown';
+				colArray = undefined;
+				rowParser = undefined;
+				if (_.size(lines) > 0) {
+					//console.log('lines[0], ', lines[0].toLowerCase());
+					//console.log('v1ColArray, ', v1ColArray.join(',').toLowerCase());
+					//console.log('v2ColArray, ', v2ColArray.join(',').toLowerCase());
+					//console.log('v3ColArray, ', v3ColArray.join(',').toLowerCase());
+					if (lines[0].toLowerCase() === v1ColArray.join(',').toLowerCase()) {
+						importVersion = 'v1';
+						colArray = v1ColArray;
+						rowParser = v1RowToObject;
+					} else if (lines[0].toLowerCase() === v2ColArray.join(',').toLowerCase()) {
+						importVersion = 'v2';
+						colArray = v2ColArray;
+						rowParser = v2RowToObject;
+					} else if (lines[0].toLowerCase() === v3ColArray.join(',').toLowerCase()) {
+						importVersion = 'v3';
+						colArray = v3ColArray;
+						rowParser = v3RowToObject;
 					}
-				});
+				}
+				return (colArray !== undefined);
+			};
 
-				var doOrgWork = function(item) {
-					return new Promise(function(rs, rj) {
-						// console.log("item:", item);
-						if (item.orgName === '') {
-							// console.log("resolving nothing for org, it was null.");
-							rs(null);
-						} else {
-							Organization.findOne ({name:item.orgName}, function (err, result) {
-								if (result === null) {
-									// console.log("Creating org:", item.orgName);
-									// Create it
-									var o = new OrganizationController(opts);
-									o.newDocument({name: item.orgName})
-									.then ( o.create )
-									.then (rs, rj);
-								} else {
-									// console.log("found the org:", result.name);
-									rs(result);
-								}
-							});
+			if (checkImportVersion()) {
+				//console.log('importing version ', importVersion);
+				var parse = new CSVParse(data, {delimiter: ',', columns: colArray}, function(err, output){
+					// Skip this many rows
+					var length = Object.keys(output).length;
+					var promises = [];
+					//console.log("length",length);
+					Object.keys(output).forEach(function(key, index) {
+						if (index > 0) {
+							var row = output[key];
+							var obj = rowParser(row);
+							if (obj) {
+								promises.push(rowParser(row));
+							}
 						}
 					});
-				};
 
-				var doUserWork = function(user, org) {
-					return new Promise(function(rs, rj) {
-						if (org) {
-							// console.log("org:", org);
-							user.org = org;
-						}
-						User.findOne ({email:user.email.toLowerCase()}, function (err, result) {
-							if (result === null) {
-								// console.log("creating:", user.email);
-								// Create it
-								var o = new User(user);
-								o.save()
-								.then(function (obj) {
-									// console.log("created:", obj);
-									rs(obj);
-								}, function (err) {
-									// console.log("err:", err);
-									rj(err);
+					var doOrgWork = function(item) {
+						return new Promise(function(rs, rj) {
+							//console.log("item:", item);
+							if (item.orgName === '') {
+								//console.log("resolving nothing for org, it was null.");
+								rs(null);
+							} else {
+								Organization.findOne ({name:item.orgName}, function (err, result) {
+									if (result === null) {
+										//console.log("Creating org:", item.orgName);
+										// Create it
+										var o = new Organization({name: item.orgName});
+										OrgCtrl.create(o).then (rs, rj);
+									} else {
+										//console.log("found the org:", result.name);
+										rs(result);
+									}
 								});
-							} else {
-								result.org = org;
-								result.save()
-								.then(rs,rj);
 							}
 						});
-					});
-				};
+					};
 
-				var doOrgToUserWork = function(user) {
-					return new Promise(function(rs, rj) {
-						if (!user.org) {
-							// console.log("no org to match.");
-							rs(null);
-						}
-						Organization.findOne({name: user.orgName})
-						.then(function (org) {
-							// Push this user into the org.
-							var index = -1;
-							for (var i=0;i<org.users.length;i++) {
-								if (org.users[i].equals(user._id)) {
-									index = i;
-									break;
+					var doUserWork = function(user, org) {
+						return new Promise(function(rs, rj) {
+							if (org) {
+								//console.log("org:", org);
+								user.org = org;
+							}
+							User.findOne ({email:user.email.toLowerCase()}, function (err, result) {
+								if (result === null) {
+									//console.log("creating:", user.email);
+									// Create it
+									var o = new User(user);
+									o.save()
+										.then(function (obj) {
+											//console.log("created:", obj);
+											rs(obj);
+										}, function (err) {
+											//console.log("err:", err);
+											rj(err);
+										});
+								} else {
+									result.org = org;
+									result.save()
+										.then(rs,rj);
 								}
-							}
-							if (index === -1) {
-								// console.log("pushign new user into org");
-								org.users.push(user._id);
-								return org.save();
-							} else {
-								// console.log("That user already existed there.");
-								return org;
-							}
-							// console.log("existing users:", org.users);
-						})
-						.then(rs, rj);
-					});
-				};
-				
-
-
-				Promise.resolve ()
-				.then (function () {
-					return promises.reduce (function (current, item) {
-						return current.then (function () {
-							return doOrgWork(item)
-							.then(function (org) {
-								return doUserWork(item, org);
-							})
-							.then(function (user) {
-								return doOrgToUserWork(user);
 							});
 						});
-					}, Promise.resolve());
-				})
-				.then (resolve, reject);
-			});
+					};
+
+					var doOrgToUserWork = function(user) {
+						return new Promise(function(rs, rj) {
+							if (!user.org) {
+								//console.log("no org to match.");
+								rs(null);
+							}
+							Organization.findOne({name: user.orgName})
+								.then(function (org) {
+									// Push this user into the org.
+									var index = -1;
+									for (var i=0;i<org.users.length;i++) {
+										if (org.users[i].equals(user._id)) {
+											index = i;
+											break;
+										}
+									}
+									if (index === -1) {
+										//console.log("pushign new user into org");
+										org.users.push(user._id);
+										return org.save();
+									} else {
+										//console.log("That user already existed there.");
+										return org;
+									}
+									//console.log("existing users:", org.users);
+								})
+								.then(rs, rj);
+						});
+					};
+
+
+
+					Promise.resolve ()
+						.then (function () {
+							return promises.reduce (function (current, item) {
+								return current.then (function () {
+									return doOrgWork(item)
+										.then(function (org) {
+											return doUserWork(item, org);
+										})
+										.then(function (user) {
+											return doOrgToUserWork(user);
+										});
+								});
+							}, Promise.resolve());
+						})
+						.then (resolve, reject);
+				});
+			} else {
+				//console.log('Unknown file import version.');
+				reject("{err: 'Unknown file import version.'}");
+			}
+
 		});
 	});
 };
@@ -273,13 +350,13 @@ exports.loadGroupUsers = function(file, req, res, opts) {
 			if (err) {
 				reject("{err: "+err);
 			}
-			// console.log("FILE DATA:",data);
+			//console.log("FILE DATA:",data);
 			var colArray = ['GROUP_ID','NAME','CONTACT_GROUP_TYPE','PERSON_ID','PROJECT_ID'];
 			var parse = new CSVParse(data, {delimiter: ',', columns: colArray}, function(err, output){
 				// Skip this many rows
 				var length = Object.keys(output).length;
 				var promises = [];
-				// console.log("length",length);
+				//console.log("length",length);
 				Object.keys(output).forEach(function(key, index) {
 					if (index > 0) {
 						var row = output[key];
@@ -392,7 +469,7 @@ exports.loadProjectUserRoles = function(file, req, res, opts) {
 					roles         : []
 				};
 				_.each(v1ColArray, function(role) {
-					console.log("role['" + role + "'] = " + row[role]);
+					//console.log("role['" + role + "'] = " + row[role]);
 					if (row[role] === 'X' || row[role] === 'x') {
 						obj.roles.push(role);
 					}
@@ -415,7 +492,7 @@ exports.loadProjectUserRoles = function(file, req, res, opts) {
 					// Skip this many rows
 					var length = Object.keys(output).length;
 					var promises = [];
-					// console.log("length",length);
+					//console.log("length",length);
 					Object.keys(output).forEach(function(key, index) {
 						if (index > 0) {
 							var row = output[key];
@@ -558,7 +635,7 @@ exports.loadSystemUserRoles = function(file, req, res, opts) {
 					// Skip this many rows
 					var length = Object.keys(output).length;
 					var promises = [];
-					// console.log("length",length);
+					//console.log("length",length);
 					Object.keys(output).forEach(function(key, index) {
 						if (index > 0) {
 							var row = output[key];
