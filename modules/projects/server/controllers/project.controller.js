900de1cb4d9579e404259be7e0bea06d37f04970
jsher <jason@sure.mn>
2017-02-03 10:52:56 -0800

    MEM-237: mines listing page hooked up and reading from the db, includes sorting on client side.

--- modules/projects/server/controllers/project.controller.js
+++ modules/projects/server/controllers/project.controller.js
@@ -31,5 +31,6 @@ module.exports = DBModel.extend ({
 				.then (resolve, reject);
 		});
 	}
+
 });
 
--- modules/projects/server/routes/project.routes.js
+++ modules/projects/server/routes/project.routes.js
@@ -8,5 +8,11 @@ var policy = require ('../../../core/server/controllers/core.policy.controller')
 module.exports = function (app) {
 	routes.setCRUDRoutes (app, 'project', Project, policy, null, {all:'guest'});
 
+	app.route ('/api/project/bycode/:projectcode')
+		.all (policy ('guest'))
+		.get (routes.setAndRun(Project, function (model, req) {
+			return model.one ({code:req.params.projectcode});
+		}));
+
 };
 
