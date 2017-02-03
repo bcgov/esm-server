88f53a03336f4cda8e45ffb5d8fd042acced1149
jsher <jason@sure.mn>
2017-02-03 14:27:51 -0800

    MEM-254: Fix to project/mine ownership import.

--- modules/projects/client/config/project.client.routes.js
+++ modules/projects/client/config/project.client.routes.js
@@ -29,9 +29,7 @@ angular.module('project').config (
 
 				$scope.ownership = function(p) {
 					try {
-						var value = p.ownership.replace(/[\r\n]/g, ";");
-						value = value.replace(/;/g, "<br>");
-						return value;
+						return p.ownership.replace(/;/g, "<br>");
 					} catch(e) {
 						return p.ownership;
 					}
