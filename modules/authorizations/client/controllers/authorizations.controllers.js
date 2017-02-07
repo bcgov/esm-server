2c85e6d66391fd7402c4582b2a15a563db320810
Bryan Gilbert <bryan.gilbert@memsharp.com>
2017-02-07 08:53:08 -0800

    MEM-234 seed db, server/client api, populate html. needs refinement

---
+++ modules/authorizations/client/controllers/authorizations.controllers.js
@@ -0,0 +1,17 @@
+// "use strict";
+//
+//
+// angular.module('inspections')
+// 	.controller('projectInspectionController', projectInspectionController);
+//
+// projectInspectionController.$inject = ['$scope', 'InspectionsModel'];
+// /* @ngInject */
+// function projectInspectionController($scope, InspectionsModel) {
+// 	var complianceList = this;
+// 	console.log("BG projectInspectionController");
+// 	InspectionsModel.forProject('brucejack').then(function (data) {
+// 		console.log("BG have data from collection", data);
+// 		complianceList.data = data;
+// 	});
+// }
+//
