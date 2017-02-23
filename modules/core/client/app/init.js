128702ffba85dc5767f76fc18092c04873bbfcbe
Mark Lise <mark.lise@gmail.com>
2017-02-23 11:55:33 -0800

    Adding new mem key for google maps

--- modules/core/client/app/init.js
+++ modules/core/client/app/init.js
@@ -18,7 +18,7 @@ angular.module(ApplicationConfiguration.applicationModuleName).config(['$locatio
 		$httpProvider.defaults.headers.get['If-Modified-Since'] = '0';
 
 		uiGmapGoogleMapApiProvider.configure({
-			key: 'AIzaSyCTbJdM2XHNQ6ybqPzyaT-242tIAgIbk8w',
+			key: 'AIzaSyDRvt9v0RfyhUgmwroWk1wlTSKBL0c7ldM',
 			v: '3.25',
 			// libraries: 'weather,geometry,visualization'
 		});
