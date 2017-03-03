7f6843da6ed4ebf689011355a5d96232715278d6
Bryan Gilbert <bryan.gilbert@memsharp.com>
2017-03-03 14:21:42 -0800

    ESM-865. Functional editing of fields. Next check impact of edits on places in use.

--- modules/code-lists/client/config/codeLists.routes.js
+++ modules/code-lists/client/config/codeLists.routes.js
@@ -1,119 +1,146 @@
-'use strict';
+(function () {
+
+	'use strict';
 // =========================================================================
 //
 // topic routes (under admin)
 //
 // =========================================================================
-angular.module('codelists').config(['$stateProvider', function ($stateProvider) {
-	$stateProvider
-	// -------------------------------------------------------------------------
-	//
-	// this is the abstract, top level view for topics.
-	// we resolve topics to all sub-states
-	//
-	// -------------------------------------------------------------------------
-	.state('admin.codelist', {
-		data: {permissions: ['manageCodeLists']},
-		abstract:true,
-		url: '/codelists',
-		template: '<ui-view></ui-view>',
-		resolve: {
-			codelists: function ($stateParams, CodeListModel) {
-				return CodeListModel.getCollection();
-			}
-		}
-	})
-	// -------------------------------------------------------------------------
-	//
-	// the list state for codeLists
-	//
-	// -------------------------------------------------------------------------
-	.state('admin.codelist.list', {
-		url: '/list',
-		templateUrl: 'modules/code-lists/client/views/code-list-list.html',
-		controller: function ($scope, $state, NgTableParams, AlertService, CodeListModel, codelists) {
-			var vm = this;
-			vm.codelists = codelists;
-			vm.currentField = codelists[0];
-			vm.tableParams = new NgTableParams ();
-			vm.changeFieldType = function(obj) {
-				vm.tableParams.settings({
-					dataset: vm.currentField.items
+	angular.module('codelists').config(['$stateProvider', function ($stateProvider) {
+		$stateProvider
+		// -------------------------------------------------------------------------
+		//
+		// this is the abstract, top level view for topics.
+		// we resolve topics to all sub-states
+		//
+		// -------------------------------------------------------------------------
+			.state('admin.codelist', {
+				data: {permissions: ['manageCodeLists']},
+				abstract: true,
+				url: '/codelists',
+				template: '<ui-view></ui-view>',
+				resolve: {
+					codelists: function ($stateParams, CodeListModel) {
+						return CodeListModel.getCollection();
+					}
+				}
+			})
+			// -------------------------------------------------------------------------
+			//
+			// the list state for codeLists
+			//
+			// -------------------------------------------------------------------------
+			.state('admin.codelist.list', {
+				url: '/:codelistName',
+				templateUrl: 'modules/code-lists/client/views/code-list-list.html',
+				controllerAs: 'vm',
+				controller: function ($scope, $state, $stateParams, $modal, NgTableParams, AlertService, CodeListModel, codelists) {
+					var vm = this;
+
+					// UI selects which code list to work with. Reload the table:
+					vm.changeFieldType = transition;
+
+					vm.codelists = codelists;
+
+					vm.codelistNames = _.pluck(vm.codelists, 'name');
+
+					vm.codelistName = $stateParams.codelistName || 'salutations';
+
+					vm.currentField = _.find(codelists, {name : vm.codelistName});
+
+					vm.nonEmptyFields = _.filter(vm.currentField.items, function (o) {
+						return o.value !== '';
+					});
+
+					vm.fields = _.sortByOrder(vm.nonEmptyFields, ['active', 'displayPriority', 'display'], ['desc', 'desc', 'asc']);
+
+					// UI invokes add or edit item within current codelist
+					vm.openAddEdit = function (item) {
+						addEditModal($scope, $modal, vm.currentField, item, onSuccess);
+					};
+
+					vm.tableParams = new NgTableParams();
+					vm.tableParams.settings({	dataset: vm.fields });
+
+					function onSuccess(item, display, displayPriority, active) {
+						var newItem = {
+							value : item ? item.value : display,
+							display : display,
+							displayPriority: displayPriority,
+							active : active
+						};
+						var others = !item ? vm.nonEmptyFields : _.filter(vm.nonEmptyFields, function (o) {
+							return o.value !== item.value;
+						});
+						others.push(newItem);
+						var newSorted = _.sortByOrder(others, ['active', 'displayPriority', 'display'], ['desc', 'desc', 'asc']);
+						vm.currentField.items = newSorted;
+						CodeListModel.save(vm.currentField)
+							.then(function () {
+								transition();
+							})
+							.catch(function (err) {
+								console.error(err);
+							});
+					}
+
+					function transition() {
+						var name = vm.currentField.name;
+						console.log("BG changeFieldType", name);
+						$state.transitionTo('admin.codelist.list', {codelistName: name }, {
+							reload: true, inherit: false, notify: true
+						});
+					}
+				}
+			});
+	}]);
+
+	function addEditModal($scope, $modal, currentField, item, onSuccess) {
+		$modal.open({
+			animation: true,
+			templateUrl: 'modules/code-lists/client/views/codelist-add-edit-modal.html',
+			controllerAs: 'vm',
+			controller: function ($scope, $modalInstance, _) {
+				var vm = this;
+				vm.title = item ? "Edit" : "Add";
+				vm.okText = item ? "Submit" : "Add";
+				vm.name = item ? item.value : '';
+				vm.display = item ? item.display : '';
+				vm.displayPriority = item ? item.displayPriority : false;
+				vm.active = item ? item.active : true;
+				vm.editMode = !! item;
+				vm.validationMessage = 'asdas';
+				vm.otherItems = _.filter(currentField.items, function(o) {
+					if(vm.editMode) {
+						return o.value !== '' && o.value !== item.value;
+					}
+					return o.value !== '';
 				});
-			};
-			// initialize table
-			vm.changeFieldType();
-		},
-		controllerAs : 'vm'
-	})
-	// -------------------------------------------------------------------------
-	//
-	// the edit state for one codelist
-	//
-	// -------------------------------------------------------------------------
-	// .state('admin.codelist.edit', {
-	// 	data: {permissions: ['manageCodeLists']},
-	// 	url: '/:id/edit',
-	// 	templateUrl: 'modules/code-list/client/views/code-list-edit.html',
-	// 	resolve: {
-	// 		topic: function ($stateParams, TopicModel) {
-	// 			return TopicModel.getModel ($stateParams.topicId);
-	// 		}
-	// 	},
-	// 	controller: function ($scope, $state, topic, TopicModel, pillars, types, codeFromTitle) {
-	// 		$scope.topic = topic;
-	// 		$scope.types = types;
-	// 		$scope.pillars = pillars;
-	// 		var which = 'edit';
-	// 		$scope.save = function (isValid) {
-	// 			if (!isValid) {
-	// 				$scope.$broadcast('show-errors-check-validity', 'topicForm');
-	// 				return false;
-	// 			}
-	// 			$scope.topic.code = codeFromTitle ($scope.topic.name);
-	// 			// console.log('parent topic = ', $scope.topic.parent);
-	// 			var p = (which === 'add') ? TopicModel.add ($scope.topic) : TopicModel.save ($scope.topic);
-	// 			p.then (function (model) {
-	// 				$state.transitionTo('admin.topic.list', {}, {
-	// 		  			reload: true, inherit: false, notify: true
-	// 				});
-	// 			})
-	// 			.catch (function (err) {
-	// 				console.error (err);
-	// 				// alert (err.message);
-	// 			});
-	// 		};
-	// 		$scope.selectTopic = function () {
-	// 			TopicModel.getTopicsForPillar ($scope.topic.pillar).then (function (topics) {
-	// 				$scope.pillartopics = topics;
-	// 				//console.log ('pillar topics = ', topics);
-	// 				//console.log ('parent topic = ', $scope.topic.parent);
-	//
-	// 				//check to see if the current parent topic is in the topics list
-	// 				//if it's not, clear it out
-	// 				var topicFound = false;
-	// 				for (var i=0; i < topics.length; i++) {
-	// 					if (topics[i].code === $scope.topic.parent) {
-   //      					topicFound = true;
-	// 						break;
-   //      				}
-   //  				}
-	//
-   //  				if(!topicFound) {
-   //  					$scope.topic.parent = '';
-   //  				}
-	//
-	// 				$scope.$apply();
-	//
-	// 			});
-	// 		};
-	// 		$scope.selectTopic ();
-	// 	}
-	// })
-	;
-}]);
 
+				vm.cancel = function () {
+					$modalInstance.dismiss('cancel');
+				};
 
+				$scope.$watch('vm.display',function(newVal, oldVal) {
+					vm.validate(newVal);
+				})
+
+				vm.validate = function (newVal) {
+					var existing = _.find(vm.otherItems, {value: newVal});
+					vm.validationMessage = existing ? newVal + " already exists" : "";
+					vm.isValid = vm.validationMessage.length === 0;
+				}
+				vm.ok = function () {
+					onSuccess(item, vm.display, vm.displayPriority, vm.active);
+					$modalInstance.close();//{resource: s.object._id, data: s.permissionRoleIndex});
+				};
+
+				// validate on the existing content
+				vm.validate(vm.display);
+			}
+		});
+	}
+})();
 
 
 
