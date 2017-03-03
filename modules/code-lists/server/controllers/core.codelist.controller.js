2f9e1c011cc776ddaec712f20ed81083e64a89e8
Bryan Gilbert <bryan.gilbert@memsharp.com>
2017-03-03 15:57:38 -0800

    ESM-865 Fix require in server controller

--- modules/code-lists/server/controllers/core.codelist.controller.js
+++ modules/code-lists/server/controllers/core.codelist.controller.js
@@ -1,6 +1,7 @@
 'use strict';
 
-var DBModel   = require ('../../core/server/controllers/core.dbmodel.controller');
+var path           = require('path');
+var DBModel        = require (path.resolve('./modules/core/server/controllers/core.dbmodel.controller'));
 
 module.exports = DBModel.extend ({
 	name: 'CodeList',
