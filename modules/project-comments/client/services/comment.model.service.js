c4d10edb48653449032dd95abef3091b6fe778bd
Bryan Gilbert <bryan.gilbert@memsharp.com>
2017-03-06 15:49:29 -0800

    ESM-845 PCP CSV download. Change date to local and use semis instead of commas in lists

--- modules/project-comments/client/services/comment.model.service.js
+++ modules/project-comments/client/services/comment.model.service.js
@@ -5,7 +5,7 @@
 // is accessed through the front end
 //
 // =========================================================================
-angular.module('comment').factory ('CommentModel', function (ModelBase, _) {
+angular.module('comment').factory ('CommentModel', function (ModelBase, moment, _) {
 	//
 	// build the model by extending the base model. the base model will
 	// have all the basic crud stuff built in
@@ -179,24 +179,30 @@ angular.module('comment').factory ('CommentModel', function (ModelBase, _) {
 					.replace (/\“/g, '"')
 					.replace (/\”/g, '"')
 					.replace (/"/g, '""');
-					if (comment.length > 30000) {
-						console.log ('comment > 32000');
-						comment = comment.substr (0,32000) + ' --- TRUNCATED FOR IMPORT TO EXCEL --- ';
+					// https://support.office.com/en-us/article/Excel-specifications-and-limits-1672b34d-7043-467e-8e27-269d656771c3
+					// actual max is 32,767
+					var MAX = 32000;
+					if (comment.length > MAX) {
+						console.log ('comment > '+ MAX);
+						comment = comment.substr (0,MAX) + ' --- TRUNCATED FOR IMPORT TO EXCEL --- ';
 					}
 					a.push (comment);
-					a.push (row.dateAdded);
+					var ts = moment(row.dateAdded);
+					var tsStr = ts.format('YYYY-MM-DDThh:mm:ssZZ');
+					a.push (tsStr);
 					a.push ((!row.isAnonymous) ? row.author : '');
 					a.push (row.location);
+					var arrayJoinChar = '; ';
 					a.push (row.pillars.map (function (v) {
 						return v.replace (/"/g, '""');
-					}).join (', '));
+					}).join (arrayJoinChar));
 					a.push (row.topics.map (function (v) {
 						return v.replace (/"/g, '""');
-					}).join (', '));
+					}).join (arrayJoinChar));
 					a.push (row.eaoStatus);
 					a.push (row.documents.map (function (v) {
 						return '""' + window.location.protocol + '//' + window.location.host + '/api/document/'+v._id+'/fetch""';
-					}).join (', '));
+					}).join (arrayJoinChar));
 
 					data += '"' + a.join ('","') + '"' + "\r\n";
 				});
