863fc3eae4e9ea0c1b980c3afafe5d0585a62a48
Mark Lise <mark.lise@gmail.com>
2017-03-27 15:14:33 -0700

    MEM-336: Disable Layers.

--- modules/maps/client/controllers/maps.clients.controllers.js
+++ modules/maps/client/controllers/maps.clients.controllers.js
@@ -22,7 +22,7 @@ function controllerMap($scope, Authentication, uiGmapGoogleMapApi, $filter, _, A
 	
 	mpl.layers = {};
 	mpl.markers = [];
-	mpl.KMLLayers = [];
+	// mpl.KMLLayers = [];
 
 	mpl.map = {
 		center: mpl.center,
@@ -56,62 +56,24 @@ function controllerMap($scope, Authentication, uiGmapGoogleMapApi, $filter, _, A
 				longitude: newValue.lon
 			});
 
-			// Go through the project's artifacts, and run through all their
-			// main documents and supporting documents.  If a KML is detected,
-			// show it.  This should behave as per what the user can/can't see
-			// with respect to their Artifact/Document privileges.
+			// // Go through the project's artifacts, and run through all their
+			// // main documents and supporting documents.  If a KML is detected,
+			// // show it.  This should behave as per what the user can/can't see
+			// // with respect to their Artifact/Document privileges.
 
-			// Mem doesn't have artifacts (yet)
-			Document.getProjectDocuments(newValue._id,false).then( function(res) {
-				_.each(res, function (doc) {
-					if (doc.internalExt === 'kml' || doc.internalExt === 'kmz') {
-						console.log("Adding layer:", doc._id);
-						var kmlURL = window.location.protocol + "//" + window.location.host + "/api/document/" + doc._id + "/fetch";
-						mpl.KMLLayers.push({
-							url: kmlURL,
-							label: doc.internalOriginalName,
-							show: true,
-							_id: doc._id
-						});
-					}
-				});
-			});
-
-			// ArtifactModel.forProject(newValue._id)
-			// .then( function(res) {
-			// 	_.each(res, function (artifact) {
-			// 		ArtifactModel.lookup(artifact._id)
-			// 		.then (function (art) {
-			// 			// Go through Main Document
-			// 			if (art.document && art.document.internalExt === 'kml') {
-			// 				var mainDocument = art.document;
-			// 				// console.log("pushing main:", art.document);
-			// 				mpl.KMLLayers.push({
-			// 					url: window.location.protocol + "//" + window.location.host + "/api/document/" + mainDocument._id + "/fetch",
-			// 					label: mainDocument.internalOriginalName,
-			// 					show: true,
-			// 					_id: mainDocument._id
-			// 				});
-			// 			}
-			// 			// Go through supporting documents
-			// 			_.each(art.supportingDocuments, function (supporting) {
-			// 				Document.getDocument(supporting)
-			// 				.then( function (res) {
-			// 					// ML: This is silly.  When documents convert to the proper DBModel,
-			// 					// this needs to change.
-			// 					var doc = res;
-			// 					if (doc && doc.internalExt === 'kml') {
-			// 						// console.log("pushing supporting doc:", doc);
-			// 						mpl.KMLLayers.push({
-			// 							url: window.location.protocol + "//" + window.location.host + "/api/document/" + doc._id + "/fetch",
-			// 							label: doc.internalOriginalName,
-			// 							show: true,
-			// 							_id: doc._id
-			// 						});
-			// 					}
-			// 				});
+			// // Mem doesn't have artifacts (yet)
+			// Document.getProjectDocuments(newValue._id,false).then( function(res) {
+			// 	_.each(res, function (doc) {
+			// 		if (doc.internalExt === 'kml' || doc.internalExt === 'kmz') {
+			// 			console.log("Adding layer:", doc._id);
+			// 			var kmlURL = window.location.protocol + "//" + window.location.host + "/api/document/" + doc._id + "/fetch";
+			// 			mpl.KMLLayers.push({
+			// 				url: kmlURL,
+			// 				label: doc.internalOriginalName,
+			// 				show: true,
+			// 				_id: doc._id
 			// 			});
-			// 		});
+			// 		}
 			// 	});
 			// });
 			$scope.showPoint = true;
