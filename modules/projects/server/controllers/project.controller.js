37ed930f1e400ebb189966c45836dde98b3b542d
Brandon <brandonzhoucharles@gmail.com>
2018-03-21 12:03:50 -0700

    em-172 allow duplicate names at different heights of tree

--- modules/projects/server/controllers/project.controller.js
+++ modules/projects/server/controllers/project.controller.js
@@ -214,12 +214,17 @@ module.exports = DBModel.extend ({
           var theNode = root.first(function (node) {
             return node.model.id === parseInt(parentId);
           });
+
           // If we found it, add it
           if (theNode) {
           // Check if this already exists.
             var bFound = theNode.first(function (node) {
-            // NB: Exclude myself
-              return (node.model.name === folderName) && (node.model.id !== theNode.model.id);
+              var nodeParentId = '';
+              if(node.parent) {
+                nodeParentId = node.parent.model.id;
+              }
+              // NB: Exclude myself
+              return (node.model.name === folderName) && (node.model.id !== theNode.model.id) && (nodeParentId === parseInt(parentId));
             });
 
             // If found, return error in creating.
