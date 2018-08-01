b6e502546ede6f1990847f80ee8a9a38fed3b51f
Nick Phura <Nick.Phura@quartech.com>
2018-08-01 16:38:44 -0700

    add mime type

--- modules/core/client/services/core.minio.services.js
+++ modules/core/client/services/core.minio.services.js
@@ -3,10 +3,17 @@
 angular.module('core').factory('MinioService', ['ModelBase', '$http', function (ModelBase, $http) {
   var MinioService = ModelBase.extend({
     getMinioPresignedPutURL: function (projectCode, fileName) {
-      return this.get('/api/getMinioPresignedPutURL/' + projectCode + '/' +fileName);
+      return this.get('/api/getMinioPresignedPutURL/' + projectCode + '/' + fileName);
     },
     putMinioPresignedPutURL: function (minioPresignedURL, file) {
-      return $http({method: 'PUT', url: minioPresignedURL, data: file});
+      return $http({
+        method: 'PUT',
+        url: minioPresignedURL,
+        data: file,
+        headers: {
+          "Content-Type": file.mimetype
+        }
+      });
     },
     removeDocument: function (fileName) {
       return this.get('/api/removeMinioDocument/' + fileName);
