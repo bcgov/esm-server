01a471ce807391ad046a65ebbf28d260a430ecf1
Emiliano Suñé <emiliano.sune@quartech.com>
2018-08-15 10:51:56 -0700

    Updated regex in updateInternalURL.js

--- scripts/minio/updateInternalURL.js
+++ scripts/minio/updateInternalURL.js
@@ -1,13 +1,13 @@
-/*global db, ObjectId */
+/*global db */
 
 /**
   * This script updates the internalURL property of each document so that it will become compatibel with Minio's bucket structure.
-  * It removes the previously gard-coded "/uploads/" heading, as with Minio the bucket name is not part of a filesystem path.
+  * It removes the previously hard-coded "/uploads/" heading, as with Minio the bucket name is not part of a filesystem path.
   */
 print('Updating...');
 var updateCount = 0;
-db.documents.find({ 'project' : ObjectId("588511caaaecd9001b825bcb")}).forEach(function(doc){
-  var newURL = doc.internalURL.replace(/^\/uploads/, '');
+db.documents.find().forEach(function(doc){
+  var newURL = doc.internalURL.replace(/^(\/)?(uploads)?\//, '');
   print('New internalUrl is: ' + newURL);
   updateCount++;
   db.documents.update({_id : doc._id}, { $set : { internalURL : newURL }});
