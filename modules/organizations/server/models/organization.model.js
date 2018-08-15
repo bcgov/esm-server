323719dd302bb59d6c72cb48209e8b065cd3f6b8
Kyle Coralejo <kylecjo@gmail.com>
2018-08-15 10:04:26 -0700

    EM-1167: formatted files

--- modules/organizations/server/models/organization.model.js
+++ modules/organizations/server/models/organization.model.js
@@ -4,27 +4,65 @@
 // Model for tasks
 //
 // =========================================================================
-module.exports = require ('../../../core/server/controllers/core.schema.controller')('Organization', {
-  __audit       : true,
-  __codename    : 'unique',
-  company       : { type: String, default:'' },
-  website       : { type: String, default:'' },
-  companyLegal  : { type: String, default:'' },
-  registeredIn  : { type: String, default:'' },
-  parentCompany : { type: String, default:'' },
-  companyType   : { type: String, default:'' },
-  address1      : { type: String, default:'' },
-  address2      : { type: String, default:'' },
-  city          : { type: String, default:'' },
-  province      : { type: String, default:'' },
-  postal        : { type: String, default:'' },
-  country       : { type: String, default:'' },
-  notes         : { type:String, default: '' },
-  users         : [
-    { type:'ObjectId', ref:'User' }
-  ]
-},[{
-  users : 1
-}]
-);
-
+module.exports = require('../../../core/server/controllers/core.schema.controller')('Organization', {
+  __audit: true,
+  __codename: 'unique',
+  company: {
+    type: String,
+    default: ''
+  },
+  website: {
+    type: String,
+    default: ''
+  },
+  companyLegal: {
+    type: String,
+    default: ''
+  },
+  registeredIn: {
+    type: String,
+    default: ''
+  },
+  parentCompany: {
+    type: String,
+    default: ''
+  },
+  companyType: {
+    type: String,
+    default: ''
+  },
+  address1: {
+    type: String,
+    default: ''
+  },
+  address2: {
+    type: String,
+    default: ''
+  },
+  city: {
+    type: String,
+    default: ''
+  },
+  province: {
+    type: String,
+    default: ''
+  },
+  postal: {
+    type: String,
+    default: ''
+  },
+  country: {
+    type: String,
+    default: ''
+  },
+  notes: {
+    type: String,
+    default: ''
+  },
+  users: [{
+    type: 'ObjectId',
+    ref: 'User'
+  }]
+}, [{
+  users: 1
+}]);
