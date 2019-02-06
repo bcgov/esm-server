51b35d965b28b50ab4218f58eb9bb1d8586b8a7e
Unknown <juliansubda@gmail.com>
2019-02-06 10:05:22 -0800

    Preliminary documentation & refactor

--- openshift/templates/admin-0-deploy-all.sh
+++ openshift/templates/admin-0-deploy-all.sh
@@ -1,39 +1,91 @@
 source ./lib/extended-oc.sh
 
-checkOpenshiftSession
+PARAMS_FOLDER=./params/VENV/
+PARAMS_FILE=admin-0.config
+ARGS_FILE=${PARAMS_FOLDER}${PARAMS_FILE}
 
-TOOLS_PROJECT=l8g1vt-tools
-TARGET_PROJECT=l8g1vt-dev
-PARAMS_FOLDER=./params/STIR/
+# ====================================================================================
+# Order dependent
+# No spaces
+# Set these in the above config file
+TOOLS_PROJECT=the-tools-project
+TARGET_PROJECT=the-target-destination-environment-project
+ADMIN_SERVER_NODEJS_BUILD=the-build-tied-to-the-github-code-branch-for-the-admin-server
+ADMIN_SERVER_NODEJS_DEPLOYMENT=the-deployment-of-the-admin-server-website
+ADMIN_SERVER_MONGODB_DEPLOYMENT=the-database-for-the-admin-server
+ADMIN_SERVER_MINIO_DEPLOYMENT=the-minio-filesystem-proxy-for-the-admin-server-filesystem
+ADMIN_SERVER_NGINX_BUILD=the-build-of-the-network-routing-proxy-for-the-admin-server
+ADMIN_SERVER_NGINX_DEPLOYMENT=the-deployment-of-the-network-routing-proxy-for-the-admin-server
+ADMIN_SERVER_BC_TEMPLATE=the-admin-server-build-config-json-template
+ADMIN_SERVER_BC_PARAMS=the-admin-server-build-config-settings
+ADMIN_SERVER_DC_TEMPLATE=the-admin-server-deployment-config-json-template
+ADMIN_SERVER_DC_PARAMS=the-admin-server-deployment-config-settings
+NGINX_BC_TEMPLATE=the-nginx-build-config-json-template
+NGINX_BC_PARAMS=the-nginx-build-config-settings
+NGINX_DC_TEMPLATE=the-nginx-deployment-config-json-template
+NGINX_DC_PARAMS=the-nginx-deployment-config-settings
+# ====================================================================================
 
-checkProjectExists ${TOOLS_PROJECT}
-checkProjectExists ${TARGET_PROJECT}
 
-ADMIN_SERVER_BC_TEMPLATE=admin-1-admin-server-on-tools.json
-ADMIN_SERVER_DC_TEMPLATE=admin-2-admin-server-on-env.json
-NGINX_BC_TEMPLATE=admin-3-nginx-on-tools.json
-NGINX_DC_TEMPLATE=admin-4-nginx-on-env.json
+runDeployAll() {
+    checkOpenshiftSession
+    checkFileExists "config" ${ARGS_FILE}
 
-checkFileExists "template", ${ADMIN_SERVER_BC_TEMPLATE}
-checkFileExists "template", ${ADMIN_SERVER_DC_TEMPLATE}
-checkFileExists "template", ${NGINX_BC_TEMPLATE}
-checkFileExists "template", ${NGINX_DC_TEMPLATE}
+    local tools_project target_project admin_server_nodejs_build admin_server_nodejs_deployment \
+        admin_server_mongodb_deployment admin_server_minio_deployment admin_server_nginx_build \
+        admin_server_nginx_deployment admin_server_bc_template admin_server_bc_params admin_server_dc_template \
+        admin_server_dc_params nginx_bc_template nginx_bc_params nginx_dc_template nginx_dc_params
+    extractArgument tools_project "runDeployAll" "TOOLS_PROJECT" "${1}"
+    extractArgument target_project "runDeployAll" "TARGET_PROJECT" "${2}"
+    extractArgument admin_server_nodejs_build "runDeployAll" "ADMIN_SERVER_NODEJS_BUILD" "${3}"
+    extractArgument admin_server_nodejs_deployment "runDeployAll" "ADMIN_SERVER_NODEJS_DEPLOYMENT" "${4}"
+    extractArgument admin_server_mongodb_deployment "runDeployAll" "ADMIN_SERVER_MONGODB_DEPLOYMENT" "${5}"
+    extractArgument admin_server_minio_deployment "runDeployAll" "ADMIN_SERVER_MINIO_DEPLOYMENT" "${6}"
+    extractArgument admin_server_nginx_build "runDeployAll" "ADMIN_SERVER_NGINX_BUILD" "${7}"
+    extractArgument admin_server_nginx_deployment "runDeployAll" "ADMIN_SERVER_NGINX_DEPLOYMENT" "${8}"
+    extractArgument admin_server_bc_template "runDeployAll" "ADMIN_SERVER_BC_TEMPLATE" "${9}"
+    extractArgument admin_server_bc_params "runDeployAll" "ADMIN_SERVER_BC_PARAMS" "${10}"
+    extractArgument admin_server_dc_template "runDeployAll" "ADMIN_SERVER_DC_TEMPLATE" "${11}"
+    extractArgument admin_server_dc_params "runDeployAll" "ADMIN_SERVER_DC_PARAMS" "${12}"
+    extractArgument nginx_bc_template "runDeployAll" "NGINX_BC_TEMPLATE" "${13}"
+    extractArgument nginx_bc_params "runDeployAll" "NGINX_BC_PARAMS" "${14}"
+    extractArgument nginx_dc_template "runDeployAll" "NGINX_DC_TEMPLATE" "${15}"
+    extractArgument nginx_dc_params "runDeployAll" "NGINX_DC_PARAMS" "${16}"
 
-ADMIN_SERVER_BC_PARAMS=${PARAMS_FOLDER}admin-1-admin-server-on-tools.params
-ADMIN_SERVER_DC_PARAMS=${PARAMS_FOLDER}admin-2-admin-server-on-env.dev.params
-NGINX_BC_PARAMS=${PARAMS_FOLDER}admin-3-nginx-on-tools.params
-NGINX_DC_PARAMS=${PARAMS_FOLDER}admin-4-nginx-on-env.dev.params
+    checkProjectExists ${tools_project}
+    checkProjectExists ${target_project}
 
-checkFileExists "parameters", ${ADMIN_SERVER_BC_PARAMS}
-checkFileExists "parameters", ${ADMIN_SERVER_DC_PARAMS}
-checkFileExists "parameters", ${NGINX_BC_PARAMS}
-checkFileExists "parameters", ${NGINX_DC_PARAMS}
+    checkFileExists "template", ${admin_server_bc_template}
+    checkFileExists "template", ${admin_server_dc_template}
+    checkFileExists "template", ${nginx_bc_template}
+    checkFileExists "template", ${nginx_dc_template}
 
-oc project ${TOOLS_PROJECT}
-oc process -f ${ADMIN_SERVER_BC_TEMPLATE} --param-file=${ADMIN_SERVER_BC_PARAMS} -n ${TOOLS_PROJECT} | oc create -f -
-oc project ${TARGET_PROJECT}
-oc process -f ${ADMIN_SERVER_DC_TEMPLATE} --param-file=${ADMIN_SERVER_DC_PARAMS} -n ${TARGET_PROJECT} | oc create -f -
-oc project ${TOOLS_PROJECT}
-oc process -f ${NGINX_BC_TEMPLATE} --param-file=${NGINX_BC_PARAMS} -n ${TOOLS_PROJECT} | oc create -f -
-oc project ${TARGET_PROJECT}
-oc process -f ${NGINX_DC_TEMPLATE} --param-file=${NGINX_DC_PARAMS} -n ${TARGET_PROJECT} | oc create -f -
+    local admin_server_bc_params_path=${PARAMS_FOLDER}${admin_server_bc_params}
+    local admin_server_dc_params_path=${PARAMS_FOLDER}${admin_server_dc_params}
+    local nginx_bc_params_path=${PARAMS_FOLDER}${nginx_bc_params}
+    local nginx_dc_params_path=${PARAMS_FOLDER}${nginx_dc_params}
+
+    checkFileExists "parameters", ${admin_server_bc_params_path}
+    checkFileExists "parameters", ${admin_server_dc_params_path}
+    checkFileExists "parameters", ${nginx_bc_params_path}
+    checkFileExists "parameters", ${nginx_dc_params_path}
+
+    echo -e \\n"deploy-all: Starting deployment."\\n
+
+    local original_namespace=$(oc project --short=true)
+
+    oc project ${tools_project}
+    oc -n ${tools_project} process -f ${admin_server_bc_template} --param-file=${admin_server_bc_params_path} | oc create -f -
+    oc project ${target_project}
+    oc -n ${target_project} process -f ${admin_server_dc_template} --param-file=${admin_server_dc_params_path} | oc create -f -
+    oc project ${tools_project}
+    oc -n ${tools_project} process -f ${nginx_bc_template} --param-file=${nginx_bc_params_path} | oc create -f -
+    oc project ${target_project}
+    oc -n ${target_project} process -f ${nginx_dc_template} --param-file=${nginx_dc_params_path} | oc create -f -
+
+    oc project ${original_namespace}
+
+    echo -e \\n"deploy-all: Completed deployment."\\n
+}
+
+runDeployAll $(<${ARGS_FILE})
